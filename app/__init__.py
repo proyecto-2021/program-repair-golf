1967954edd50bc60ca69fe1b2b62c6202d2d15bb
SE UNRC <fbrusatti@dc.exa.unrc.edu.ar>
2021-10-01 18:49:56 -0300

    Configure SQLAlchemy. Ref #4

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,8 @@
 from flask import Flask
 from config import config
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
 
 def create_app(config_name='development'):
     app = Flask(__name__)
@@ -7,6 +10,8 @@ def create_app(config_name='development'):
     app.config.from_object(config[config_name])
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     config[config_name].init_app(app)
+
+    db.init_app(app)
 
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint, url_prefix='/auth')
