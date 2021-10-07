aeaeee9f2362e4b374adb3ab4245a722e24d72b2
jpbn456 <jpbortol@gmail.com>
2021-10-07 20:18:27 -0300

    #40 draft

--- app/javascript/views.py
+++ app/javascript/views.py
@@ -1,6 +1,12 @@
 from . import javascript
+from flask import jsonify, make_response
+from .models import JavaScriptChallenges
 
 @javascript.route('/login')
 def login():
     return { 'result': 'javascript' }
 
+@javascript.rout('/api/v1/javascript-challenges', methods=['GET'])
+def javascipt_challenges():
+    challenges = JavaScriptChallenges.query(id,code,repair_objective,complexity).all()
+    return make_response(jsonify({"challenges" : [challenges]}), 200)
