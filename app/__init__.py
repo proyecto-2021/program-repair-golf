1fa66d95d9ec89a071adea3fc41240bfcf33b243
Jplferreyra <jplferreyra@gmail.com>
2021-10-07 17:57:58 -0300

    Made RubyChallenge visible for alembic. Ref #11

--- app/__init__.py
+++ app/__init__.py
@@ -14,6 +14,7 @@ def create_app(config_name='development'):
     config[config_name].init_app(app)
 
     from .models import User, Role
+    from .ruby.models import RubyChallenge
 
     db.init_app(app)
     migrate.init_app(app, db)
