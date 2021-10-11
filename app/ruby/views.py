970c1310291d425084f901b1881cffe4951fd5eb
Gonzalo-FloresM <floresgo42@gmail.com>
2021-10-10 23:54:16 -0300

    create RubyChallenge object and dictionary to return. Ref #13

--- app/ruby/views.py
+++ app/ruby/views.py
@@ -1,7 +1,22 @@
 from . import ruby
-#Some imports will be needed to use database
+from .models import RubyChallenge
+from app import db
+from flask import Flask, jsonify, request, make_response
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine
+import json
 
-@ruby.route('/<string:name>')
-def ruby_hello(name):
-    return 'Hello ' + name
+@ruby.route('/ruby-challenges', methods=['POST'])
+def create_ruby_challenge():
 
+	aux_challenge = json.loads(request.form.get('challenge'))
+	aux_dict = aux_challenge['challenge']
+	new_challenge = RubyChallenge(
+		code = request.form.get('source_code_file'),
+		tests_code = request.form.get('test_suite_file'),
+		repair_objective = aux_dict['repair_objective'],
+		complexity = aux_dict['complexity'],
+		best_score = 0
+	)
+
+	return jsonify({'challenge': new_challenge.get_dict()})
