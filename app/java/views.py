5eba93ea9643f5d97e8c9378ca5669ff7b1f9b47
ritazabala <ritazabala28@gmail.com>
2021-10-11 16:48:10 -0300

    Create-GET-all-Java-challenges

--- app/java/views.py
+++ app/java/views.py
@@ -1,5 +1,18 @@
+from app.java.models_java import Challenge_java
+from flask import jsonify, make_response
 from . import java
 
 @java.route('/prueba')
 def login():
     return { 'result': 'funciona' }
+
+# GET 'http://localhost:4000/api/v1/java-challenges'
+@java.route('/java-challenges',methods=['GET'])
+def ViewAllAsignments():
+    challenge = {"challenges":[]}
+    challenge ['challenges']=Challenge_java.query.all()
+    all_challenges=[]
+    for i in challenge['challenges']:
+        all_challenges.append(Challenge_java.__repr__(i))
+    return make_response(jsonify({"challenges":all_challenges}))
+    
