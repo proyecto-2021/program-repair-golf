956cf2d10f879f591b71538ec6a04e97b0d65586
ritazabala <ritazabala28@gmail.com>
2021-10-12 19:54:13 -0300

    #34 Create-GET-all-Java-Challenges

--- app/java/views.py
+++ app/java/views.py
@@ -7,12 +7,14 @@ def login():
     return { 'result': 'funciona' }
 
 # GET 'http://localhost:4000/api/v1/java-challenges'
-@java.route('/java-challenges',methods=['GET'])
-def ViewAllAsignments():
+@java.route('/api/v1/java-challenges',methods=['GET'])
+def ViewAllChallenges():
     challenge = {"challenges":[]}
-    challenge ['challenges']=Challenge_java.query.all()
+    challenge ['challenges'] = Challenge_java.query.all()
     all_challenges=[]
     for i in challenge['challenges']:
-        all_challenges.append(Challenge_java.__repr__(i))
+        aux_challenge = Challenge_java.__repr__(i)
+        #aux_challenge.pop('tests_code',None)
+        all_challenges.append(aux_challenge)
     return make_response(jsonify({"challenges":all_challenges}))
     
