e1c74caa5bf721902f763972ffbd631155008eaf
Luchit0-afk <lucianopalacio91@gmail.com>
2021-10-13 19:33:44 -0300

    Jsonify path. Ref #30

--- app/go/views.py
+++ app/go/views.py
@@ -1,11 +1,11 @@
-
-
+from flask import Flask,jsonify,request,make_response
 from . import go
 
 @go.route('/hello') 
 def hello():
     return 'Hello World!'
 
-@go.route('api/v1/go-challenges/<int:id>/repair')
+@go.route('api/v1/go-challenges/<int:id>/repair', methods=['POST'])
 def repair_challengue_go(id):
-    return 'test'
+    path_to_file = request.form.get('source_code_file')
+    return jsonify(path_to_file)
