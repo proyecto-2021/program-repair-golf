e67d34cf0d5cf08046b993d700f015c9ff7bafeb
virginiacattaneo <virginiacattaneo80@gmail.com>
2021-10-13 20:22:46 -0300

    create get single java challenges ref.#33

--- app/java/views.py
+++ app/java/views.py
@@ -1,5 +1,17 @@
 from . import java
+from app.java.models_java import Challenge_java
+from flask import jsonify, make_response
 
 @java.route('/prueba')
 def login():
-    return { 'result': 'funciona' }
+    return { 'result': 'funcionaok' }
+
+
+# Get Assignment by ID
+@java.route('/java-challenges/<int:id>',methods=['GET'])
+def View_Challenges(id):
+    Challenge=Challenge_java.query.filter_by(id=id).first()
+    if (Challenge is None):
+        return make_response(jsonify({"challenge":"Not found prueba"}),404)   
+    else: 
+        return make_response(jsonify({"challenge":[Challenge_java.__repr__(Challenge)]}))   
