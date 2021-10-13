702b26c02cf58e9fc2ba87ff56ba3363f28ea6d9
Luchit0-afk <lucianopalacio91@gmail.com>
2021-10-13 19:12:21 -0300

    Initial configure method. Ref #30

--- app/go/views.py
+++ app/go/views.py
@@ -1,5 +1,11 @@
+
+
 from . import go
 
 @go.route('/hello') 
 def hello():
     return 'Hello World!'
+
+@go.route('api/v1/go-challenges/<int:id>/repair')
+def repair_challengue_go(id):
+    return 'test'
