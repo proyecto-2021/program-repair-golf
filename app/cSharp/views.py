129df519820330641ceb8ffc96a44167d2c0fd2a
RubenDanielle <daniellerubenr@gmail.com>
2021-10-13 12:07:32 -0300

    Define GET challenges route and method. #19

--- app/cSharp/views.py
+++ app/cSharp/views.py
@@ -3,3 +3,8 @@ from . import cSharp
 @cSharp.route('/login')
 def login():
     return { 'result': 'Ok' }
+
+@cSharp.route('/c-sharp-challenges', methods=['GET'])
+def get_csharp_challenges():
+    # [TODO]: implement this method
+    return {'challenges': 'Unsupported View'}
