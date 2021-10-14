6175a715dafd8a601ed817a5ec918af9f9ca5892
EAMolina500 <molinaenzoagustin@gmail.com>
2021-10-14 15:06:28 -0300

    get_all method - first version

--- app/go/views.py
+++ app/go/views.py
@@ -1,5 +1,26 @@
 from . import go
+from .models_go import GoChallenge
+from app import db
+from flask import Flask, jsonify, request, make_response
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine
+from werkzeug.datastructures import FileStorage
+import json
 
 @go.route('/hello') 
 def hello():
     return 'Hello World!'
+
+
+@go.route('/api/v1/go-challenges', methods=['GET'])
+def get_all_challenges():
+	challenges = db.session.query(GoChallenge).all()
+	show = []
+	for i in challenges:
+		show.append(GoChallenge.convert_dict(i))
+	return jsonify({"challenges" : show})
+
+'''
+@app.route('api/v1/go-challenges/<int:id>', methods=['PUT'])
+def update_challenge(id):
+'''
