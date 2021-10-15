0afc2df9bd2de4c5d2798448948713cd1fc5184b
Lucrecia Mascanfroni <lucreciamascanfroni@MacBook-Air-de-Lucrecia.local>
2021-10-15 17:27:23 -0300

    Change view to only return code and tests_code paths. #20

--- app/cSharp/views.py
+++ app/cSharp/views.py
@@ -10,6 +10,5 @@ def login():
 @cSharp.route('/c-sharp-challenges/<int:id>', methods = ['GET'])
 def get_challenge(id):
     challenge = db.session.query(CSharp_Challenge).get(id).__repr__()
-    challenge['code'] = open(challenge['code'], "r").read()
-    challenge['tests_code'] = open(challenge['tests_code'], "r").read()
     return jsonify({ 'Challenge': challenge })
+
