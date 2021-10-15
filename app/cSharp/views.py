cfc9fd3f5e24708bff0dea86e1229f1dc735f7ed
ayelen01 <garciiaayelen@gmail.com>
2021-10-15 18:09:09 -0300

    add method's definition. #23

--- app/cSharp/views.py
+++ app/cSharp/views.py
@@ -1,5 +1,14 @@
 from . import cSharp
+from app import db
+from .models import CSharp_Challenge
+from flask import jsonify
+from flask import make_response
 
 @cSharp.route('/login')
 def login():
     return { 'result': 'Ok' }
+
+@cSharp.route('/c-sharp-challenges/<int:id>', methods=['PUT'])
+def put_csharp_challenges():
+    #[TODO]: implement this method
+    return {'challenges': 'Unsupported View'}
