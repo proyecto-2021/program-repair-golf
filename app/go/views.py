9021a94b9c1d5463e76a36e8d636393465338ceb
giung1 <agus_98_jov@hotmail.com.ar>
2021-10-15 19:54:00 -0300

    modified and almost done. Ref #27

--- app/go/views.py
+++ app/go/views.py
@@ -1,27 +1,16 @@
 from . import go
-from models_go import GoChallenge
 from flask import request, jsonify
 from .. import db
+from .models_go import GoChallenge
 
 @go.route('/hello') 
 def hello():
     return 'Hello World!'
 
 @go.route('/api/v1/go-challenges/<id>', methods=['GET'])
-    def return_single_challenge(id):
-        challenge_by_id=GoChallenge.query.filter_by(name=id).first()
+def return_single_challenge(id):
+        challenge_by_id=GoChallenge.query.filter_by(id=id).first()
         if challenge_by_id is None:
             return "ID Not Found", 404
-        #code=open(str(challenge_by_id.code),'r')
-        #code_for_dict=""
-        #for linea in code:
-        #    code_for_dict + linea +"\n"
-        challenge_to_return={
-        #    "code":code_for_dict,
-            "code":challenge_by_id.code,
-            "tests_code":challenge_by_id.tests_code,
-            "repair_objective":challenge_by_id.repair_objective,
-            "complexity":challenge_by_id.complexity,
-            "score":challenge_by_id.score,
-        }
+        challenge_to_return=challenge_by_id.convert_dict()
         return jsonify({"challenge":challenge_to_return})
