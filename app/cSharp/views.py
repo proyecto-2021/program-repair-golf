80c17b3974a316174e6d2abd8f5235e5e6f8b5ed
Lucrecia Mascanfroni <lucreciamascanfroni@MacBook-Air-de-Lucrecia.local>
2021-10-15 00:25:36 -0300

    Add definition for method. #20

--- app/cSharp/views.py
+++ app/cSharp/views.py
@@ -3,3 +3,8 @@ from . import cSharp
 @cSharp.route('/login')
 def login():
     return { 'result': 'Ok' }
+
+@cSharp.route('/c-sharp-challenges/<int:id>', methods = ['GET'])
+def get_challenge(id):
+    #TODO: implement this method
+    return{ 'challenge': 'unsupported' }
