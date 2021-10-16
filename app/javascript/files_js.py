9f530e3770a15870caef8c18034855e6c8a21ff8
Mati36 <lopezmatias36@gmail.com>
2021-10-16 16:24:57 -0300

    method that returns the name of a file when passing the path. Ref #99

--- app/javascript/files_js.py
+++ app/javascript/files_js.py
@@ -4,6 +4,8 @@ from pathlib import PurePath, PurePosixPath
 PUBLIC_PATH = './public/challenges/' 
 FILE_EXTENSION = '.js'
 
+def get_name_file(path):
+    return PurePosixPath(path).stem
 
 def valid(file):
     return  file and PurePath(file.filename).suffix == FILE_EXTENSION
@@ -13,6 +15,9 @@ def upload(file,file_name):
     if not os.path.lexists(PUBLIC_PATH):
         os.makedirs(PUBLIC_PATH)
     
+    if not file_name: 
+        file = 'dafault_example'
+
     path = PUBLIC_PATH + file_name + FILE_EXTENSION
     
     if os.path.lexists(path) and os.path.isfile(path):
