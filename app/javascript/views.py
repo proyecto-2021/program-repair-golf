b733831d02b73227608894cc69257dd0a2a76055
LucianoOga <luchovh17@gmail.com>
2021-10-16 17:16:32 -0300

    The method was created create challenge. Ref #43

--- app/javascript/views.py
+++ app/javascript/views.py
@@ -1,27 +1,10 @@
 from . import javascript
-from .models_js import JavaScriptChallenge
-from flask import Flask, jsonify, request
-from .. import db
-import json
+from .create_challenge import create_challenge_js
 
 @javascript.route('/login')
 def login():
     return { 'result': 'javascript' }
 
-
 @javascript.route('/javascript-challenges', methods=['POST'])
-def createChallengeJS():
-  #challenge_json = request.get_json() if request.get_json() else None
-  
-  challenge_json = json.loads(request.form.get('challenge'))
-
-  newChallengeJS = JavaScriptChallenge(code = request.form.get('source_code_file'),
-                                      tests_code = request.form.get('test_suite_file'),
-                                      repair_objective = challenge_json['challenge']['repair_objective'],
-                                      complexity = challenge_json['challenge']['complexity'],
-                                      best_score = 0)
-    
-  db.session.add(newChallengeJS)
-  db.session.commit()
-  
-  return jsonify({'challenge': newChallengeJS.get_dict()})
+def create_challenge():
+  return create_challenge_js()
