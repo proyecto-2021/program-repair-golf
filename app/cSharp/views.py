a88684f097af6c29e32134fd0060298088d5b50a
tomas98Fer <tomas016fernandez@gmail.com>
2021-10-18 19:58:32 -0300

    add method's definition. #22

--- app/cSharp/views.py
+++ app/cSharp/views.py
@@ -3,3 +3,8 @@ from . import cSharp
 @cSharp.route('/login')
 def login():
     return { 'result': 'Ok' }
+
+@cSharp.route('c-sharp-challenges/:id/repair', methods=['POST'])
+def repair_Candidate(id):
+    #to do : implement this method
+    pass
