4a0922201c0f79d4c63b78831fe66355c28788c3
giung1 <agus_98_jov@hotmail.com.ar>
2021-10-18 20:47:10 -0300

    first commit for the modifying. Ref #106

--- app/go/views.py
+++ app/go/views.py
@@ -1,5 +1,24 @@
 from . import go
+from flask import request, jsonify
+from .. import db
+from .models_go import GoChallenge
 
 @go.route('/hello') 
 def hello():
     return 'Hello World!'
+
+@go.route('/api/v1/go-challenges/<id>', methods=['GET'])
+def return_single_challenge(id):
+        challenge_by_id=GoChallenge.query.filter_by(id=id).first()
+        if challenge_by_id is None:
+            return "ID Not Found", 404
+        challenge_to_return=challenge_by_id.convert_dict()
+        del challenge_to_return["id"]
+        return jsonify({"challenge":challenge_to_return})
+
+
+def from_file_to_str(path):
+    file= open(path,'r')
+    content=file.readlines()
+    file.close()
+    return content
