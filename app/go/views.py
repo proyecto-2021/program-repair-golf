0f3fc474170430dcfcd8d0a75f3af97d89dc5edd
giung1 <agus_98_jov@hotmail.com.ar>
2021-10-19 20:11:29 -0300

    Issues with newlines of the path's code but almost working. Ref #106

--- app/go/views.py
+++ app/go/views.py
@@ -14,13 +14,14 @@ def return_single_challenge(id):
         if challenge_by_id is None:
             return "ID Not Found", 404
         challenge_to_return=challenge_by_id.convert_dict()
-        challenge_to_return["code"]="".join(from_file_to_str(os.path.abspath(challenge_to_return["code"])))
+        from_file_to_str(challenge_to_return)
         del challenge_to_return["id"]
         return jsonify({"challenge":challenge_to_return})
 
 
-def from_file_to_str(path):
-    file= open(str(path),'r')
+def from_file_to_str(challenge):
+    file= open(str(os.path.abspath(challenge["code"])),'r')
     content=file.readlines()
     file.close()
-    return content
+    challenge["code"]="".join(content)
+    return challenge
