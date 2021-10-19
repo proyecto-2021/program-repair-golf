17651d39e0085b91de17a21f5bf287c3eb9197f5
Ayelen Garcia <garciiaayelen@gmail.com>
2021-10-19 19:29:48 -0300

    [#23]add check if there is a challenge with that id

--- app/cSharp/views.py
+++ app/cSharp/views.py
@@ -10,5 +10,11 @@ def login():
 
 @cSharp.route('/c-sharp-challenges/<int:id>', methods=['PUT'])
 def put_csharp_challenges():
-    #[TODO]: implement this method
-    return {'challenges': 'Unsupported View'}
+    challenge= CSharp_Challenge.query.filter_by(id=id).first()
+    data_Challenge = loads(request.form.get('challenge'))['challenge']
+
+    if challenge is None:
+        return make_response(jsonify({"challenge":"There is no challenge for this id"}), 404)
+
+
+
