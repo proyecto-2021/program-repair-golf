177133907adfa97ce13f0edb04b1236f6d13e2d2
iriurri <urricelquiirina@gmail.com>
2021-10-19 20:17:53 -0300

    add modules used. add verification of file path for repair candidate. #22

--- app/cSharp/views.py
+++ app/cSharp/views.py
@@ -2,6 +2,8 @@ from . import cSharp
 from app import db
 from .models import CSharp_Challenge
 from  flask import jsonify,make_response,json,request
+import subprocess,os
+
 
 @cSharp.route('/login')
 def login():
@@ -12,8 +14,13 @@ def repair_Candidate(id):
     #to do : implement this method
     # verify challenge's existence 
     if db.session.query(CSharp_Challenge).get(id) is not None:
-        archivo = open(request.json('source_code_file'),'r')
-        # check if repair candidate passes test. If this happens then calculate score.
+        challenge = db.session.query(CSharp_Challenge).get(id).__repr__()
+        if os.path.exists(request.get_json()['source_code_file']):
+            with open(request.json('source_code_file'),'r') as repair_file:
+                # check if repair candidate passes test. If this happens then calculate score.
+                pass
+        else:
+            return make_response(jsonify({'repair challenge': 'Not found'}),404)
     else: 
         return make_response(jsonify({'challenge': 'Not found'}),404)
     pass
