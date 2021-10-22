286291f70cecc0d21e04baf5aa5e49a5c68ed39c
nachoo <nachogonz2702@yahoo.com.ar>
2021-10-22 20:02:06 -0300

    Added new methods and implemented temporal file saving #98

--- app/python/views.py
+++ app/python/views.py
@@ -15,13 +15,11 @@ def return_challenges():
     all_challenges = PythonChallenge.query.all()
     challenge_list = []
     for challenge in all_challenges:
-        
         aux_dict = PythonChallenresponse.to_dict(challenge)
-        saved_challenge = open(aux_dict['code'], "r")
-        challenge_code = saved_challenge.read()
-        aux_dict['code'] = challenge_code
+        aux_dict['code'] = read_file(aux_dict['code'], "r")
         aux_dict.pop('tests_code', None)
         challenge_list.append(aux_dict)
+
     return jsonify({"challenges": challenge_list})
 
 @python.route('/api/v1/python-challenges/<id>', methods=['GET'])
@@ -30,15 +28,11 @@ def return_challange_id(id):
     if challenge is None:
         return make_response(jsonify({"Challenge": "Not found"}), 404)
     aux_dict = PythonChallenge.to_dict(challenge)  
-    saved_challenge = open(aux_dict['code'], "r")   
-    challenge_code = saved_challenge.read()
-    saved_challenge.close()
-    aux_dict['code'] = challenge_code
-    saved_challenge2 = open(aux_dict['tests_code'], "r")
-    challenge_test_code = saved_challenge2.read()
-    saved_challenge2.close()
-    aux_dict['tests_code'] = challenge_test_code
+    
+    aux_dict['code'] = read_file(aux_dict['code'], "r")
+    aux_dict['tests_code'] = read_file(aux_dict['tests_code'], "r")
     aux_dict.pop('id', None)
+
     return jsonify({"Challenge": aux_dict}) 
 
 @python.route('/api/v1/python-challenges', methods=['POST'])
@@ -80,43 +74,17 @@ def create_new_challenge():
 def update_challenge(id):
     challenge_data = loads(request.form.get('challenge'))['challenge']
     save_to = "public/challenges/"  #general path were code will be saved
-    temp_path = "public/temp/"      #general path to temp directory
-
     req_challenge = PythonChallenge.query.filter_by(id=id).first()
     
     if req_challenge is None:   #case id is not in database
         return make_response(jsonify({"challenge":"there is no challenge with that id"}),404)
 
     response = PythonChallenge.to_dict(req_challenge).copy()   #start creating response for the endpoint
-    #determine path to temp, user may have requested name change for the file
-    code_path = temp_path
-    if 'source_code_file_name' in challenge_data:
-        code_path += challenge_data['source_code_file_name']
-    else:   #concatenate old file name
-        code_path += (lambda x: x.split('/')[-1]) (req_challenge.code)
     
-    #same as above
-    test_path = temp_path
-    if 'test_suite_file_name' in challenge_data:
-        test_path += challenge_data['test_suite_file_name']
-    else:   #concatenate old file name
-        test_path += (lambda x: x.split('/')[-1]) (req_challenge.code)
-
-    #check if code change was requested
-    challenge_file = request.files.get('source_code_file')  #obtain the binary
-    if challenge_file != None:
-        challenge_source_code = challenge_file.read()   #read it, and store its content
-        saved_challenge = open(req_challenge.code, "wb")   #creating a new file in new location
-        saved_challenge.write(challenge_source_code)    #write the binary we got
-        saved_challenge.close()
-    
-    #check if test code change was requested
-    tests_file = request.files.get('test_suite_file')
-    if tests_file != None:
-        tests_source_code = tests_file.read()
-        saved_challenge = open(req_challenge.tests_code, "wb")
-        saved_challenge.write(challenge_source_code)
-        saved_challenge.close()
+    new_code = request.files.get('source_code_file')
+    new_test = request.files.get('test_suite_file')
+    if file_changes_required(challenge_data, new_code, new_test):
+        temporary_save(challenge_data, new_code, new_test, req_challenge.code, req_challenge.tests_code)
 
     #check if change for repair objective was requested
     if 'repair_objective' in challenge_data:
@@ -135,9 +103,7 @@ def read_and_resave(save_to, req_key, filename):
     bin_file = request.files.get(req_key)               #obtain the binary
     source_code = bin_file.read()                       #read it, and store its content
     full_path = path.join(save_to, filename)          #save_path + file_name
-    new_save = open(full_path, "wb")                    #creating a new file in new location
-    new_save.write(source_code)                         #write the binary we got
-    new_save.close()                                    #save it
+    save_file(full_path, "wb", source_code)
     return source_code
 
 
@@ -157,3 +123,46 @@ def valid_python_challenge(code_path,test_path):
 def no_syntax_errors(code_path):
     p = subprocess.call("python -m py_compile " + code_path ,stdout=subprocess.PIPE, shell=True)
     return p == 0   #0 is no syntax errors, 1 is the opposite
+
+#checks for name or content change reuqest
+def file_changes_required(names, code, tests):
+    return code is None or tests is None or 'source_code_file_name' in names or 'test_suite_file_name' in names
+
+def temporary_save(names, code_file, tests_file, old_code_path, old_test_path):
+    temp_path = "public/temp/"      #path to temp directory
+    
+    code_path = determine_path(names.get('source_code_file_name'), temp_path, old_code_path)
+    test_path = determine_path(names.get('test_suite_file_name'), temp_path, old_test_path)
+
+    source_code = None
+    if code_file != None:
+        source_code = code_file.read()   #read it, and store its content
+    else:
+        source_code = read_file(old_code_path, "rb")
+    save_file(code_path, "wb", source_code)
+
+    source_code_tests = None
+    if tests_file != None:
+        source_code_tests = tests_file.read()   #read it, and store its content
+    else:
+        source_code_tests = read_file(old_test_path, "rb")
+    save_file(test_path, "wb", source_code_tests)
+    
+
+def read_file(path, mode):
+    file = open(path, mode)
+    content = file.read()
+    file.close()
+    return content
+
+def save_file(path, mode, content):
+    file = open(path, mode)
+    file.write(content)
+    file.close()
+
+#returns a new path, if no filename, takes an old name from a path
+def determine_path(filename, base_path, old_path):
+    if filename is None:
+        return base_path + (lambda x: x.split('/')[-1]) (old_path)
+    else:
+        return base_path + filename
