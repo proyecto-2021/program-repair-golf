94741664caaaf9e20465e44df4e0982f8ffe5796
Luchit0-afk <lucianopalacio91@gmail.com>
2021-10-22 19:35:19 -0300

    Checking a sintaxis the solution file. Ref #30

--- app/go/views.py
+++ app/go/views.py
@@ -1,11 +1,25 @@
 from flask import Flask,jsonify,request,make_response
 from . import go
 
+import subprocess
+import os
+
 @go.route('/hello') 
 def hello():
     return 'Hello World!'
 
 @go.route('api/v1/go-challenges/<int:id>/repair', methods=['POST'])
 def repair_challengue_go(id):
-    path_to_file = request.form.get('source_code_file')
-    return jsonify(path_to_file)
+    code_solution_file = request.files['source_code_file']
+    code_solution_path = 'public/challenges/code_solution.go'
+    #Save the candidate solution for later delete this
+    code_solution_file.save(code_solution_path)
+    
+    is_good_code_solution_file = subprocess.run(["go","build",code_solution_path],stderr=subprocess.STDOUT, stdout=subprocess.DEVNULL)
+    os.remove(code_solution_path)
+    
+    #return jsonify(is_good_code_solution_file.returncode)
+    if is_good_code_solution_file.returncode == 2:
+        return make_response((jsonify({"code_solution_file":"with errors"}),409))
+    
+    return make_response((jsonify({"code_solution_file":"without errors"}),409))
