888cc5a281dafda3a3f24fb8b6f598bb1b723ccf
nachoo <nachogonz2702@yahoo.com.ar>
2021-10-23 15:57:55 -0300

    Adding import, methods moved #145

--- app/python/views.py
+++ app/python/views.py
@@ -5,6 +5,7 @@ from .models import PythonChallenge
 from json import loads
 from os import path
 import subprocess
+from .file_utils import *
 
 @python.route('/login', methods=['GET'])
 def login():
@@ -54,9 +55,12 @@ def create_new_challenge():
     result = valid_python_challenge(code_path, test_path)
     if 'Error' in result:
         return make_response(jsonify(result), 409)
-
-    challenge_source_code = read_and_resave(save_to, 'source_code_file', challenge_data['source_code_file_name'])
-    tests_source_code = read_and_resave(save_to, 'test_suite_file', challenge_data['test_suite_file_name'])
+    #save in new path
+    challenge_source_code = request.files.get('source_code_file').read()
+    save_file(code_path, "wb", challenge_source_code)
+    #save in new path
+    tests_source_code = request.files.get('test_suite_file').read()
+    save_file(test_path, "wb", tests_source_code)
    
     #create row for database with the new challenge
     new_challenge = PythonChallenge(code=code_path,
@@ -105,15 +109,6 @@ def update_challenge(id):
 
     return jsonify({"challenge" : response})
 
-
-def read_and_resave(save_to, req_key, filename):
-    bin_file = request.files.get(req_key)               #obtain the binary
-    source_code = bin_file.read()                       #read it, and store its content
-    full_path = path.join(save_to, filename)          #save_path + file_name
-    save_file(full_path, "wb", source_code)
-    return source_code
-
-
 def valid_python_challenge(code_path,test_path):
     #checks for any syntax errors in code
     if not no_syntax_errors(code_path):
@@ -139,41 +134,3 @@ def  tests_fail(test_path):
 def file_changes_required(names, code, tests):
     return code is None or tests is None or 'source_code_file_name' in names or 'test_suite_file_name' in names
 
-def temporary_save(names, code_file, tests_file, old_code_path, old_test_path):
-    temp_path = "public/temp/"      #path to temp directory
-    
-    code_path = determine_path(names.get('source_code_file_name'), temp_path, old_code_path)
-    test_path = determine_path(names.get('test_suite_file_name'), temp_path, old_test_path)
-
-    source_code = None
-    if code_file != None:
-        source_code = code_file.read()   #read it, and store its content
-    else:
-        source_code = read_file(old_code_path, "rb")
-    save_file(code_path, "wb", source_code)
-
-    source_code_tests = None
-    if tests_file != None:
-        source_code_tests = tests_file.read()   #read it, and store its content
-    else:
-        source_code_tests = read_file(old_test_path, "rb")
-    save_file(test_path, "wb", source_code_tests)
-    
-
-def read_file(path, mode):
-    file = open(path, mode)
-    content = file.read()
-    file.close()
-    return content
-
-def save_file(path, mode, content):
-    file = open(path, mode)
-    file.write(content)
-    file.close()
-
-#returns a new path, if no filename, takes an old name from a path
-def determine_path(filename, base_path, old_path):
-    if filename is None:
-        return base_path + (lambda x: x.split('/')[-1]) (old_path)
-    else:
-        return base_path + filename
