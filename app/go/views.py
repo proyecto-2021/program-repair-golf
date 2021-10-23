1a31cd91dbaeaf3eea7277c3b4a52b686c3df700
Luchit0-afk <lucianopalacio91@gmail.com>
2021-10-23 09:42:50 -0300

    Checking if the solution code pass the test. Ref #30

--- app/go/views.py
+++ app/go/views.py
@@ -1,8 +1,12 @@
+import json
 from flask import Flask,jsonify,request,make_response
+
+from app.go.models_go import GoChallenge
 from . import go
 
 import subprocess
 import os
+import shutil
 
 @go.route('/hello') 
 def hello():
@@ -11,14 +15,32 @@ def hello():
 @go.route('api/v1/go-challenges/<int:id>/repair', methods=['POST'])
 def repair_challengue_go(id):
     code_solution_file = request.files['source_code_file']
-    code_solution_path = 'public/challenges/code_solution.go'
+    code_solution_path = 'public/challenges/solution/code_solution.go'
     #Save the candidate solution for later delete this
     code_solution_file.save(code_solution_path)
     
     is_good_code_solution_file = subprocess.run(["go","build",code_solution_path],stderr=subprocess.STDOUT, stdout=subprocess.DEVNULL)
-    os.remove(code_solution_path)
+    
     
     if is_good_code_solution_file.returncode == 2:
         return make_response((jsonify({"code_solution_file":"with errors"}),409))
     
-    return make_response((jsonify({"code_solution_file":"without errors"}),409))
+    challengue = GoChallenge.query.filter_by(id=id).first()
+    challengue_to_dict = challengue.convert_dict()
+    tests_code = challengue_to_dict["tests_code"]
+    
+    #Falta eliminar esto
+    shutil.copy (tests_code,"public/challenges/solution/code_test.go")
+    
+    #Falta probar bien
+    the_challenge_is_solved = subprocess.run(["go","test"],cwd="public/challenges/solution",stderr=subprocess.STDOUT, stdout=subprocess.DEVNULL)
+    #1 cuando fallan los test
+    if the_challenge_is_solved.returncode == 1:
+        return make_response((jsonify({"the challengue":"not solved"}),409))
+    
+    
+    
+    #print(type(challengue_to_dict))
+    #os.remove(code_solution_path)
+    return jsonify(the_challenge_is_solved.returncode)
+    #return make_response((jsonify({"code_solution_file":"without errors"}),409))
