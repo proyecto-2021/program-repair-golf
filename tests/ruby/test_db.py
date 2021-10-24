1a9c071610714ca58f49138f111f3f13a00cf80f
Agustin Nolasco <franconolasco99@gmail.com>
2021-10-24 19:51:00 -0300

    Correct tests. Ref #117

--- tests/ruby/test_db.py
+++ tests/ruby/test_db.py
@@ -9,31 +9,24 @@ def generate_challenge():
         tests_code='tests_code',
         repair_objective='repair_objective',
         complexity='5',
-        best_score=0
     )
-    
+
     return challenge
 
 def test_create_challenge(client):
-    challenge = generate_challenge()
-    RubyChallenge.create_challenge(challenge)
-
+    RubyChallenge.create_challenge('code', 'tests_code', 'repair_objective', '5')
     assert len(RubyChallenge.query.all()) == 1
 
 def test_get_challenge(client):
-    challenge1 = RubyChallenge.get_challenge(1)
-    RubyChallenge.create_challenge(generate_challenge())
-    challenge2 = RubyChallenge.get_challenge(2)
-
-    assert challenge1.get_dict()['id'] == 1
-    assert challenge2.get_dict()['id'] == 2
+    challenge = RubyChallenge.create_challenge('code', 'tests_code', 'repair_objective', '5')
+    assert challenge['id'] == 2
 
 def test_get_all_challenges(client):
     challenges_list = RubyChallenge.get_challenges()
     assert len(challenges_list) == 2
     
     for i in range(10):
-        RubyChallenge.create_challenge(generate_challenge())
+        RubyChallenge.create_challenge('code', 'tests_code', 'repair_objective', '5')
     
     challenges_list = RubyChallenge.get_challenges()
     assert len(challenges_list) == 12
