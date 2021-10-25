9673216231a3d04fef3111dd3c68826e1e800396
giung1 <agus_98_jov@hotmail.com.ar>
2021-10-25 18:10:50 -0300

    new method to fix GET single  added. Ref #157

--- app/go/views.py
+++ app/go/views.py
@@ -15,14 +15,21 @@ def return_single_challenge(id):
             return "ID Not Found", 404
         challenge_to_return=challenge_by_id.convert_dict()
         from_file_to_str(challenge_to_return)
+        from_file_to_str_tests(challenge_to_return)
         del challenge_to_return["id"]
         return jsonify({"challenge":challenge_to_return})
 
 
 def from_file_to_str(challenge):
     file= open(str(os.path.abspath(challenge["code"])),'r')
-    content=file.readlines()
+    content=file.readlines ()
     file.close()
-    challenge["code"]="".join(content)
+    challenge["code"]=(content)
     return challenge
 
+def from_file_to_str_tests(challenge):
+    file= open(str(os.path.abspath(challenge["tests_code"])),'r')
+    content=file.readlines()
+    file.close()
+    challenge["tests_code"]=(content)
+    return challenge
