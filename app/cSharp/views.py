d22beb0e124896253e77b483196fecb401eb83a6
iriurri <urricelquiirina@gmail.com>
2021-10-26 17:28:54 -0300

    add path and sintax checks, add NUnit libs. #22

--- app/cSharp/views.py
+++ app/cSharp/views.py
@@ -4,7 +4,11 @@ from app import db
 from .models import CSharp_Challenge
 from  flask import jsonify,make_response,json,request
 import subprocess,os
+from subprocess import PIPE
 
+NUNIT_PATH="./app/cSharp/lib/NUnit.3.13.2/lib/net35/"
+NUNIT_LIB="./app/cSharp/lib/NUnit.3.13.2/lib/net35/nunit.framework.dll"
+NUNIT_CONSOLE_RUNNER="./app/cSharp/lib/NUnit.ConsoleRunner.3.12.0/tools/nunit3-console.exe"
 
 @cSharp.route('/login')
 def login():
@@ -12,27 +16,27 @@ def login():
 
 @cSharp.route('c-sharp-challenges/<int:id>/repair', methods=['POST'])
 def repair_Candidate(id):
-    #to do : implement this method
     # verify challenge's existence 
     if db.session.query(CSharp_Challenge).get(id) is not None:
         challenge = db.session.query(CSharp_Challenge).get(id).__repr__()
+        challenge_name = os.path.basename(challenge['code'])
         file = request.files['source_code_file']
-        file.save('public/challenges')
-        file_name = os.path.basename(file)
-        cmd = 'mcs ' + 'public/challenges/' + file_name
+        path = 'public/challenges/' + challenge_name
+        file.save(dst=path)
+        cmd = 'mcs ' + path
         if (subprocess.call(cmd, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT) == 0):
             test = challenge['tests_code']
-            test_file_name = os.path.basename(test)
-            #commands to run tests
-            cmd_export = 'export MONO_PATH=./NUnit.3.13.2/lib/net35/'
-            cmd_compile = 'mcs ' + file_name + ' ' + test_file_name + ' -target:library -r:NUnit.3.13.2/lib/net35/nunit.framework.dll -out:' + test_file_name.replace('.cs', '.dll')
-            cmd_test = 'mono ./NUnit.ConsoleRunner.3.12.0/tools/nunit3-console.exe ' + test_file_name.replace('.cs', '.dll') + ' -noresult'
-            cmd_run_test = 'cmd_export && cmd_compile && cmd_test' 
-            if (subprocess.call(cmd_run_test, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT) == 0):
+            #commands to run tests. Don't work
+            #cmd_export = 'export MONO_PATH=' + NUNIT_PATH
+            #cmd_compile = cmd + ' ' + test + ' -target:library -r:' + NUNIT_LIB + ' -out:' + test.replace('.cs', '.dll')
+            #cmd_execute = 'mono ' + NUNIT_CONSOLE_RUNNER + ' ' + test.replace('.cs', '.dll') + ' -noresult'
+            #cmd_run_test = cmd_export + ' && ' + cmd_compile + ' && ' + cmd_execute 
+            #if (subprocess.call(cmd_run_test, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT) == 0):
                 #to do: calculate score
-                return make_response(jsonify({'Repair candidate:' : 'Tests passed'}), 200)
-            else:
-                return make_response(jsonify({'Repair candidate:' : 'Tests not passed'}), 409)
+            #    return make_response(jsonify({'Repair candidate:' : 'Tests passed'}), 200)
+            #else:
+            #    return make_response(jsonify({'Repair candidate:' : 'Tests not passed'}), 409)
+            return make_response(jsonify({'repair candidate:' : 'compiled'}), 200)
         else:
             return make_response(jsonify({'repair candidate:' : 'Sintax error'}), 409)
 
