541a9702d70696811e6d390ad004be61d0829239
tomas98Fer <tomas016fernandez@gmail.com>
2021-10-27 18:12:08 -0300

    add implement to client method. #163

--- tests/cSharp/__init__.py
+++ tests/cSharp/__init__.py
@@ -3,6 +3,13 @@ import pytest
 
 @pytest.fixture(scope='module')
 def client():
-    #to do: implement this method
-    pass
+    # Arrange
+    app = create_app('testing')
+    # Create a test client using the Flask application configured for testing
+    with app.test_client() as test_client:
+        # Establish an application context
+        with app.app_context():
+            db.create_all()
+            # Tests will be executed on the test_client object
+            yield test_client
  
