c91de7923be5a550becc4e2fb7ec696eb761eac6
luchox73 <luchox73x@gmail.com>
2021-10-26 21:02:43 -0300

    delete get_all_challenges_dict metod. Ref #115

--- app/ruby/models.py
+++ app/ruby/models.py
@@ -23,11 +23,7 @@ def get_challenge(id):
     return db.session.query(RubyChallenge).filter_by(id=id).first().get_dict()
 
 def get_challenges():
-    return [challenge.get_dict() for challenge in
-        db.session.query(RubyChallenge).all()]
-
-def get_all_challenges_dict():
-    return list(map(lambda x: x.get_dict(), get_challenges()))
+    return [challenge.get_dict() for challenge in db.session.query(RubyChallenge).all()]
 
 def create_challenge(code, tests_code, repair_objective, complexity):
     challenge = RubyChallenge(
