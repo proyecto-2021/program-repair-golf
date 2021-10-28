09f6b5c3ea5018761d204e5057bb89bd80f6c361
ritazabala <82459669+ritazabala@users.noreply.github.com>
2021-10-28 20:20:41 -0300

    Update test_basics_db.py

--- tests/java/test_basics_db.py
+++ tests/java/test_basics_db.py
@@ -7,5 +7,11 @@ from . import client
 #Create Challenge
 #def test_new_challenge(client):
 
+def test_ViewAllChallenges(client):
+    challenge = {"challenges":[]}
+    challenge ['challenges'] = Challenge_java.query.all()
+    res = len(challenge)
+    assert  res == 1
+    #comportamiento esperado
 
    
