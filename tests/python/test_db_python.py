8ce57d1a5ceb8b32d5e1c08e27609957fc5ae97f
matiasg99 <matiias.gomeez999@gmail.com>
2021-10-28 19:39:54 -0300

    add more test of to db  #167

--- tests/python/test_db_python.py
+++ tests/python/test_db_python.py
@@ -41,8 +41,37 @@ def test_db_delete_all_data(client):
   
   assert len(PythonChallenge.query.all()) == lengDbBefore
 
+#testing of update field complexity
+def test_db_update_field(client):
+  
+  db.session.query(PythonChallenge).delete()
+  db.session.commit()
 
+  new_challenge = PythonChallenge(
+      code='code',
+      tests_code='test_path',
+      repair_objective= "Make all tests pass.",
+      complexity= "1",
+      best_score=0)
+  
+  db.session.add(new_challenge)
+  db.session.commit()
 
+  db.session.query(PythonChallenge).filter(PythonChallenge.id == new_challenge.id).update(dict( complexity = "100") )
+  db.session.commit()
 
+  db.session.query(PythonChallenge).filter(PythonChallenge.id == new_challenge.id).update(dict( best_score = 32 ) )
+  db.session.commit()
 
+  db.session.query(PythonChallenge).filter(PythonChallenge.id == new_challenge.id).update(dict( repair_objective = "testing field" ) )
+  db.session.commit()
+
+  
+  complexityRes = PythonChallenge.query.filter_by(complexity = "100" ).first()
+  
+  assert complexityRes.complexity == 100
+  assert complexityRes.best_score == 32
+  assert complexityRes.repair_objective == "testing field"
+
+  
 
