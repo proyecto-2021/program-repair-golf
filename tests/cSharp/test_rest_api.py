5fbc4c7b3615d49a23265bef9749e73ab21a152b
tomas98Fer <tomas016fernandez@gmail.com>
2021-10-29 16:39:57 -0300

    Add modularization in repair. #164

--- tests/cSharp/test_rest_api.py
+++ tests/cSharp/test_rest_api.py
@@ -1,5 +1,11 @@
 from . import client
 
 def test_post_repair(client):
-	#to do implement  this method.
+	#Arrange
+	#path_test='/test-files/MedianRepair.cs'
+	#file_repair = {'source_code_file': open(path_test, 'r')}
+	#Act
+
+
+	#Assert
 	pass  
