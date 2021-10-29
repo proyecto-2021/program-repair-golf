d25b2112a76abe62740b5b457eccd2b297b22da4
LucianoOga <luchovh17@gmail.com>
2021-10-29 13:03:41 -0300

    A method was created to compile and run tests. Challenge_api class was created to interact with the DB. Ref #173

--- app/javascript/models_js.py
+++ app/javascript/models_js.py
@@ -1,4 +1,8 @@
 from .. import db
+from ..javascript import files_controller
+from flask import jsonify, make_response
+from pathlib import PurePath, PurePosixPath
+
 class JavascriptChallenge(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     code = db.Column(db.String(256))
@@ -19,3 +23,30 @@ class JavascriptChallenge(db.Model):
 
     def find_challenge(id_challenge):
         return JavascriptChallenge.query.filter_by(id=id_challenge).first()
+    
+    """
+    version1
+    def compileRunTest(file_path): #Problem Types... ver eso
+        compiles_out = files_controller.compile_js(code_file_path)
+
+        if compiles_out:
+            files_controller.remove_files(code_file_path,test_file_path)
+            return make_response(jsonify({'challenge': f'Error File not compile {compiles_out}'}), 404)
+
+        test_out = files_controller.run_test(test_file_path)   
+        if not files_controller.test_fail(test_out):
+            files_controller.remove_files(code_file_path,test_file_path)
+            return make_response(jsonify({'challenge': f'The test has to fail at least once {test_out}'}), 404)
+    
+    """
+    """
+    version2
+    def compileRunTest(path_file): #Problem Types.. ver
+        compiles_out_err = files_controller.compile_js(file_test_path_upd)
+        test_out = files_controller.run_test(file_test_path_upd)
+
+        if compiles_out_err or not files_controller.test_fail(test_out):
+            files_controller.remove_files(code_path_upd, test_path_upd)
+            err = f'Error File not compile {compiles_out_err}' if compiles_out_err else f'The test has to fail at least once {test_out}'
+            return make_response(jsonify({'challenge': f'Error File not compile {err}'}), 404)
+    """
