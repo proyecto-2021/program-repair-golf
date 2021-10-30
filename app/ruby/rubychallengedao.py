2043d29327af862a11c3e026d0c1b16aa0f36359
Agustin Nolasco <franconolasco99@gmail.com>
2021-10-30 18:50:10 -0300

    Add attr and methods to class RubyChallengeDAO. Ref #180

--- app/ruby/rubychallengedao.py
+++ app/ruby/rubychallengedao.py
@@ -1,33 +1,34 @@
+from .models import RubyChallenge
+from app import db
+
 class RubyChallengeDAO(object):
-	"""docstring for RubyChallengeDAO"""
-	def __init__(self, arg):
-		super(RubyChallengeDAO, self).__init__()
-		self.arg = arg
-	
-	def get_challenge(id):
-    return db.session.query(RubyChallenge).filter_by(id=id).first().get_dict()
+	def __init__(self):
+		pass
 
-	def get_challenges():
-    	return [challenge.get_dict() for challenge in db.session.query(RubyChallenge).all()]
+	def get_challenge(self, id):
+		return db.session.query(RubyChallenge).filter_by(id=id).first()
 
-	def create_challenge(code, tests_code, repair_objective, complexity):
-    	challenge = RubyChallenge(
+	def get_challenges(self):
+		return [challenge.get_dict() for challenge in db.session.query(RubyChallenge).all()]
+
+	def create_challenge(self, code, tests_code, repair_objective, complexity):
+		challenge = RubyChallenge(
     	    code = code,
     	    tests_code = tests_code,
     	    repair_objective = repair_objective,
     	    complexity = complexity,
     	    best_score = 0
     	)
-    	db.session.add(challenge)
-    	db.session.commit()
-    	return challenge.get_dict()
+		db.session.add(challenge)
+		db.session.commit()
+		return challenge.get_dict()
 
-	def update_challenge(id, changes):
- 	   if len(changes) == 0:
- 	       return 1
- 	   result = db.session.query(RubyChallenge).filter_by(id=id).update(changes)
- 	   db.session.commit()
- 	   return result
+	def update_challenge(self, id, changes):
+		if len(changes) == 0:
+			return 1
+		result = db.session.query(RubyChallenge).filter_by(id=id).update(changes)
+		db.session.commit()
+		return result
 
-	def exists(id):
- 	   return get_challenge(id) is not None
+	def exists(self, id):
+		return get_challenge(id) is not None
