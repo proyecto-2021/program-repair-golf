a459230d2a803adb0e197677fdb4e20da64a1665
Agustin Nolasco <franconolasco99@gmail.com>
2021-10-30 19:47:26 -0300

    Add method save to RubyCode class. Ref #180

--- app/ruby/rubycode.py
+++ app/ruby/rubycode.py
@@ -1,9 +1,10 @@
-import subprocess
+import os, subprocess
 
 class RubyCode(object):
-	def __init__(self, path, file_name):
+	def __init__(self, path, file_name, file):
 		self.path = path
 		self.file_name = file_name
+		self.file = file
 
 	def get_path(self):
 		return self.path
@@ -13,6 +14,12 @@ class RubyCode(object):
 
 	def get_full_name(self):
 		return self.path + self.file_name
+
+	def save(self):
+		if os.path.isfile(self.get_full_name()):
+			return False
+		self.file.save(dst=get_full_name())
+		return True
 
 	def get_content(self):
 		with open(self.get_full_name()) as f:
