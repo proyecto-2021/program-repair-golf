1190c645d6e3f598e10d78149cd1efa1e15793ee
Agustin Nolasco <franconolasco99@gmail.com>
2021-10-30 18:09:46 -0300

    Define the principal attributes and methods of class RubyChallenge. Ref #180

--- app/ruby/rubychallenge.py
+++ app/ruby/rubychallenge.py
@@ -1,6 +1,24 @@
+import subprocess, sys
+
 class RubyChallenge(object):
-	"""docstring for RubyChallenge"""
-	def __init__(self, arg):
-		super(RubyChallenge, self).__init__()
-		self.arg = arg
-		
+	def __init__(self, code, tests_code, repair_objective, complexity):
+		self.code = code
+		self.tests_code = tests_code
+		self.repair_objective = repair_objective
+		self.complexity = complexity
+
+	def tests_fail(self):
+		command = 'ruby ' + self.code.get_path()
+		return subprocess.call(command, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT) != 0
+
+	def dependencies_ok(self):
+		command = 'grep "require_relative" ' + self.tests_code.get_path()
+		p = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
+		dependence_name = (p.communicate()[0].decode(sys.stdout.encoding).strip().split("'")[1])
+		return dependence_name == self.code.name()
+
+	def code_compiles(self):
+		return self.code.compiles()
+
+	def tests_code_compiles(self):
+		return self.tests_code.compiles()
