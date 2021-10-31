e7445a9b58497ab83e5138c800f494e44aa7befe
Agustin Nolasco <franconolasco99@gmail.com>
2021-10-31 18:55:33 -0300

    Add new method and attributes to clase RubyChallenge. Ref #180

--- app/ruby/rubychallenge.py
+++ app/ruby/rubychallenge.py
@@ -1,11 +1,13 @@
 import subprocess, sys
 
-class RubyChallenge(object):
-	def __init__(self, code, tests_code, repair_objective, complexity):
+class RubyChallenge:
+	def __init__(self, id,code, tests_code, repair_objective, complexity, best_score):
+		self.id = id
 		self.code = code
 		self.tests_code = tests_code
 		self.repair_objective = repair_objective
 		self.complexity = complexity
+		self.best_score = best_score
 
 	def tests_fail(self):
 		command = 'ruby ' + self.code.get_full_name()
@@ -26,3 +28,13 @@ class RubyChallenge(object):
 	def is_valid(self):
 		return self.code_compiles() and self.tests_code_compiles() \
 			   and self.tests_fail() and self.tests_fail()
+
+	def get_content(self):
+		return {
+			'id': self.id,
+			'code': self.code.get_content(),
+			'tests_code': self.tests_code.get_content(),
+			'repair_objective': self.repair_objective,
+			'complexity': self.complexity,
+			'best_score': self.best_score
+		}
