cebc4468c78ef5831116059e6e010751ce83ed80
nachoo <nachogonz2702@yahoo.com.ar>
2021-10-31 18:25:34 -0300

    Added challenge validation to PythonChallenge #191

--- app/python/PythonChallenge.py
+++ app/python/PythonChallenge.py
@@ -1,3 +1,5 @@
+from subprocess_utils import valid_python_challenge
+
 class PythonChallenge:
 
   def __init__(self, challenge_data, code, code_path, test, test_path):
@@ -7,4 +9,5 @@ class PythonChallenge:
     self.complexity = challenge_data.get('complexity')
 
   def is_valid():
-    #To-do
+    return valid_python_challenge(code.path, test.path)
+  
