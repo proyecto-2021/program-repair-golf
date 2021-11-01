ecb556be6723a4719cd4d2b0e20521a8872ed0ab
LucianoOga <luchovh17@gmail.com>
2021-11-01 14:33:30 -0300

    Modification of command_module and CommandRunException. Ref #181

--- app/javascript/modules/command_module.py
+++ app/javascript/modules/command_module.py
@@ -1,15 +1,14 @@
 import subprocess 
-from ..exceptions.CommandRunException import CommandRunException
 
 
 def run_command(command):
-    proc = subprocess.run(command, shell = True, stdout = subprocess.PIPE, stderr = subprocess.STDOUT)
-    if  proc!= 0:
-        raise CommandRunException("Command error:",404)
-    return proc
+    return subprocess.run(command, shell = True, stdout = subprocess.PIPE, stderr = subprocess.STDOUT)
     
 def command_output(pro):
     return str(pro.stdout)
+
+def run_command_ok(proc):
+    return proc.returncode != 0
     
 
 
