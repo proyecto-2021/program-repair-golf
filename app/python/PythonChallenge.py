7fb0b9b035b897dc5bbe95ec24c51082beec0933
nachoo <nachogonz2702@yahoo.com.ar>
2021-11-02 19:54:20 -0300

    Added multiple constructor and a method to return challenge like json #191

--- app/python/PythonChallenge.py
+++ app/python/PythonChallenge.py
@@ -1,13 +1,34 @@
-from subprocess_utils import valid_python_challenge
+from .PythonSourceCode import PythonSourceCode
 
 class PythonChallenge:
 
-  def __init__(self, challenge_data, code, code_path, test, test_path):
-    self.code = PythonSourceCode(code, code_path)
-    self.test = PythonSourceCode(test, test_path)
-    self.repair_objective = challenge_data.get('repair_objective')
-    self.complexity = challenge_data.get('complexity')
+  def __init__(self, **data):
+    challenge_data = data['challenge_data']
+    code = data.get('code')
+    
+    if code is None: #if no explicit code, then we have the path
+      self.code = PythonSourceCode(path=challenge_data.code)
+      self.test = PythonSourceCode(path=challenge_data.tests_code)
+      self.repair_objective = challenge_data.repair_objective
+      self.complexity = challenge_data.complexity
+    else: #we have code but not path for it
+      self.code = PythonSourceCode(code=data['code'], name=challenge_data['source_code_file_name'])
+      self.test = PythonSourceCode(code=data['test'], name=challenge_data['test_suite_file_name'])
+      self.repair_objective = challenge_data['repair_objective']
+      self.complexity = challenge_data['complexity']
 
   def is_valid():
     return valid_python_challenge(code.path, test.path)
   
+  #if content is true the json will contain code, otherwise it will contain the paths of code
+  def to_json(self, content = True):
+    challenge_data = {'repair_objective': self.repair_objective, 'complexity': self.complexity}
+    
+    if content:
+      challenge_data['code'] = self.code.get_content()
+      challenge_data['tests_code'] = self.test.get_content()
+    else:
+      challenge_data['code'] = self.code.path
+      challenge_data['tests_code'] = self.test.path
+
+    return challenge_data
