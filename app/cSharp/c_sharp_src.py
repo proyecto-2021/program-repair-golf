eb63298b34c4fd502df2e75d0c586def36ca42e0
RubenDanielle <daniellerubenr@gmail.com>
2021-11-04 01:16:51 -0300

    add method to verify test compilation. #228

--- app/cSharp/c_sharp_src.py
+++ app/cSharp/c_sharp_src.py
@@ -3,6 +3,9 @@ from subprocess import PIPE
 
 class CSharpSrc:
 
+	NUNIT_PATH = "./app/cSharp/lib/NUnit.3.13.2/lib/net35/"
+	NUNIT_LIB = "./app/cSharp/lib/NUnit.3.13.2/lib/net35/nunit.framework.dll"
+
 	def __init__(self, code_file, path, file_name):
 		self.code_file = code_file
 		self.path = path
@@ -11,3 +14,11 @@ class CSharpSrc:
 	def compiles(self):
 		compile_code_cmd = 'mcs ' + self.path
 		return subprocess.call(compile_code_cmd, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT) == 0
+
+	def test_compiles(self, test):
+		compile_code_cmd = 'mcs ' + self.path
+		test_dll = test.path.replace('.cs', '.dll')
+		cmd_export = 'export MONO_PATH=' + self.NUNIT_PATH
+		test_compile_cmd = compile_code_cmd + ' ' + test.path + ' -target:library -r:' + self.NUNIT_LIB + ' -out:' + test_dll
+		final_cmd = cmd_export + ' && ' + test_compile_cmd
+		return subprocess.call(final_cmd, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT) == 0
