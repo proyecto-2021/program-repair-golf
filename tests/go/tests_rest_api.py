501787f48f604e9ac10c723c3e6bec1282df5d21
Luchit0-afk <lucianopalacio91@gmail.com>
2021-11-04 11:20:57 -0300

    Complete the get id test. Ref #234

--- tests/go/tests_rest_api.py
+++ tests/go/tests_rest_api.py
@@ -12,10 +12,17 @@ def test_getId_for_id_correct(client):
 
     #act
     ret_pos = client.post("/api/v1/go-challenges",json=challengue)
-    ret_get = client.get(f"/api/v1/go-challenges/{ret_pos.json().id}")
+    ret_get = client.get(f"/api/v1/go-challenges/{ret_pos.json()['id']}")
 
     #assert
-    assert ret_get.status_code == 200
-    assert ret_get.json.id == ret_pos.json.id
+    ret_pos_json = ret_pos.json()
+    ret_get_json = ret_get.json()
 
+    assert ret_get.status_code == 200
+    assert ret_get_json.json()["id"] == ret_pos_json.json()["id"]
+    assert ret_get_json.json()["code_path"] == ret_pos_json.json()["code_path"]
+    assert ret_get_json.json()["tests_code"] == ret_pos_json.json()["tests_code"]
+    assert ret_get_json.json()["repair_objective"] == ret_pos_json.json()["repair_objective"]
+    assert ret_get_json.json()["complexity"] == ret_pos_json.json()["complexity"]
+    assert ret_get_json.json()["best_score"] == ret_pos_json.json()["best_score"]
 
