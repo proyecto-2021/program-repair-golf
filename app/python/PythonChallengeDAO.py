0643224ec0980e0c618b1a0c70f2bc320387554f
nachoo <nachogonz2702@yahoo.com.ar>
2021-11-03 22:34:41 -0300

    Changed create_challenge to take a PythonChallenge and return id #191

--- app/python/PythonChallengeDAO.py
+++ app/python/PythonChallengeDAO.py
@@ -1,3 +1,5 @@
+from .. import db
+from .PythonChallenge import PythonChallenge
 from .models import PythonChallengeModel
 
 class PythonChallengeDAO:
@@ -8,15 +10,16 @@ class PythonChallengeDAO:
   def get_challenge(id):
     return PythonChallengeModel.query.filter_by(id = id).first()
 
-  def create_challenge(code_path, test_path, repair_objective, complexity):
-    new_challenge = PythonChallengeModel(code=new_code_path,
-        tests_code=new_tests_path,
-        repair_objective=challenge_data['repair_objective'],
-        complexity=challenge_data['complexity'],
+  def create_challenge(challenge: PythonChallenge):
+    new_challenge = PythonChallengeModel(code=challenge.code_path(),
+        tests_code=challenge.test_path(),
+        repair_objective=challenge.repair_objective,
+        complexity=challenge.complexity,
         best_score=0)
 
     db.session.add(new_challenge)
     db.session.commit()
+    return new_challenge.id
 
   def update_challenge(id, new_data):
     db.session.query(PythonChallengeModel).filter_by(id=id).update(dict(new_data))
