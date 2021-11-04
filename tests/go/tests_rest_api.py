7b001be84b4b3cce585f5dc52f5ab0037488dff6
Luchit0-afk <lucianopalacio91@gmail.com>
2021-11-04 11:07:14 -0300

    Add a test for check when pass a correct id. Ref #234

--- tests/go/tests_rest_api.py
+++ tests/go/tests_rest_api.py
@@ -2,5 +2,20 @@ import os
 import pytest
 from app import create_app, db
 from . import client
+from app.go.models_go import GoChallenge
+
+
+def test_getId_for_id_correct(client):
+    # arrange
+    challengue = GoChallenge(code="code_path", tests_code="test_path", repair_objective="repair", complexity="1",
+                             best_score=100)
+
+    #act
+    ret_pos = client.post("/api/v1/go-challenges",json=challengue)
+    ret_get = client.get(f"/api/v1/go-challenges/{ret_pos.json().id}")
+
+    #assert
+    assert ret_get.status_code == 200
+    assert ret_get.json.id == ret_pos.json.id
 
 
