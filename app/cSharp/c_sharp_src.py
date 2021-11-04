36774bf5341e365553bbf47cfc7c09a637d15a9f
RubenDanielle <daniellerubenr@gmail.com>
2021-11-04 00:53:29 -0300

    add compile method to verify if C Sharp code compiles. #228

--- app/cSharp/c_sharp_src.py
+++ app/cSharp/c_sharp_src.py
@@ -1,8 +1,13 @@
-class CSharpSrc:
+import subprocess, os
+from subprocess import PIPE
 
-	compile_code_cmd = 'mcs ' + self.path
+class CSharpSrc:
 
 	def __init__(self, code_file, path, file_name):
 		self.code_file = code_file
 		self.path = path
 		self.file_name = file_name
+
+	def compiles(self):
+		compile_code_cmd = 'mcs ' + self.path
+		return subprocess.call(compile_code_cmd, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT) == 0
