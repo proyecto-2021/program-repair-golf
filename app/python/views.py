9b23c14bd55cd4eebc234ccbede28d0f4d196162
nachoo <nachogonz2702@yahoo.com.ar>
2021-11-03 23:01:03 -0300

    Now usign Controller for post and added error catching on get #191

--- app/python/views.py
+++ app/python/views.py
@@ -16,49 +16,24 @@ class PythonViews(MethodView):
         if id is None:
             challenge_list = PythonController.get_all_challenges()
             return jsonify({"challenges": challenge_list})
-        
         else:
             response = PythonController.get_single_challenge(id)
+            if 'Error' in response:
+                return make_response(jsonify(response), 409)
+
             return jsonify({"Challenge": response}) 
 
     def post(self):
-        #we get the dict with keys "source_code_file_name", "test_suite_file_name", "repair_objective", "complexity"
+        #gather data for post
         challenge_data = loads(request.form.get('challenge'))['challenge']
-        save_to = "public/challenges/"  #general path were code will be saved
-        saved_at = "example-challenges/python-challenges/"  #general path were code is saved
-
-        code_path = saved_at + challenge_data['source_code_file_name']
-        test_path = saved_at + challenge_data['test_suite_file_name']
-        result = valid_python_challenge(code_path, test_path)
-        if 'Error' in result:
-            return make_response(jsonify(result), 409)
-        #save in new path
         challenge_source_code = request.files.get('source_code_file').read()
-        new_code_path = save_to + challenge_data['source_code_file_name']
-        save_file(new_code_path, "wb", challenge_source_code)
-        #save in new path
         tests_source_code = request.files.get('test_suite_file').read()
-        new_tests_path = save_to + challenge_data['test_suite_file_name']
-        save_file(new_tests_path, "wb", tests_source_code)
-    
-        #create row for database with the new challenge
-        new_challenge = PythonChallengeModel(code=new_code_path,
-            tests_code=new_tests_path,
-            repair_objective=challenge_data['repair_objective'],
-            complexity=challenge_data['complexity'],
-            best_score=0)
 
-        db.session.add(new_challenge)
-        db.session.commit()
+        post_result = PythonController.post_challenge(challenge_data, challenge_source_code, tests_source_code)
+        if 'Error' in post_result:
+            return make_response(jsonify(post_result), 409)
 
-        #create response
-        req_challenge = PythonChallengeModel.query.filter_by(id=new_challenge.id).first()
-        response = PythonChallengeModel.to_dict(req_challenge)
-        #adding source codes to response
-        response['code'] = challenge_source_code.decode()
-        response['tests_code'] = tests_source_code.decode()
-        return jsonify({"challenge" : response})
-
+        return post_result
 
     def put(self, id):
         challenge_data = request.form.get('challenge')
