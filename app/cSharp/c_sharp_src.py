33c6223ced21b1f2f37dc5fd786878db70b8d91d
RubenDanielle <daniellerubenr@gmail.com>
2021-11-04 00:35:09 -0300

    Add file_name argument to init. #228

--- app/cSharp/c_sharp_src.py
+++ app/cSharp/c_sharp_src.py
@@ -2,8 +2,7 @@ class CSharpSrc:
 
 	compile_code_cmd = 'mcs ' + self.path
 
-	def __init__(self, code_file, path):
+	def __init__(self, code_file, path, file_name):
 		self.code_file = code_file
 		self.path = path
-
-
+		self.file_name = file_name
