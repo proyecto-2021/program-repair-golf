72cc022efd6b1d858643caa836e9334b2fc5f23e
matiasg99 <matiias.gomeez999@gmail.com>
2021-11-03 22:27:03 -0300

    implement test to endpoint 'GET' total #167

--- tests/python/test_endPoint_python.py
+++ tests/python/test_endPoint_python.py
@@ -24,3 +24,37 @@ def test_post_pythonChallenge(client):
 
     response = client.post('http://localhost:5000/python/api/v1/python-challenges', data=dataChallenge)
     assert response.status_code == 200
+
+def test_get_total_pythonChallenge(client):
+    result = client.get('http://localhost:5000/python/api/v1/python-challenges')
+    
+    #data to be entered in the post test
+    dataEnteredPost = {'challenges': [{'best_score': 0, 'code': '\ndef median(a,b,c):\n    res = 0\n    if ((a>=b and a<=c) or (a>=c and a<=b)):\n        res = a\n    if ((b>=a and b<=c) or (b>=c and b<=a)):\n        res = b\n    else:\n        res = c\n    return res\n\n', 'complexity': 1, 'id': 1, 'repair_objective': 'make all test pass'}]}
+
+    #data obtained through the get ready for manipulation
+    dataChallenge = parseDataTextAJson(result.json)
+
+    #I get each value within the dictionary
+    repair_objective = dataChallenge['challenges'][0]['repair_objective']
+    best_score       = dataChallenge['challenges'][0]['best_score']
+    complexity       = dataChallenge['challenges'][0]['complexity']
+    code             = dataChallenge['challenges'][0]['code']
+
+
+    assert isinstance(complexity,int) == True
+    assert len(repair_objective) > 0
+    assert len(code) > 0
+    assert result.json == dataEnteredPost 
+    
+
+
+
+
+
+def parseDataTextAJson(data):
+    dataResultText = json.dumps(data)
+
+    dataResultJson = json.loads(dataResultText)
+
+    return dataResultJson
+
