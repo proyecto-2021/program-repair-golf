e46aa899ad47d86bbc7b6c195a5c9ccf5febc2e0
nachoo <nachogonz2702@yahoo.com.ar>
2021-11-05 12:11:58 -0300

    Repair merged in view class #191

--- app/python/views.py
+++ app/python/views.py
@@ -9,6 +9,7 @@ from json import loads
 from os import path
 from .file_utils import *
 from .subprocess_utils import *
+import nltk
 
 class PythonViews(MethodView):
 
@@ -51,7 +52,62 @@ class PythonViews(MethodView):
 
         return jsonify({"challenge" : update_result})
 
+    def repair_challenge(id):
+        #Challenge in db 
+        challenge = PythonChallenge.query.filter_by(id=id).first()
+        if challenge is None:
+            return make_response(jsonify({"Challenge": "Not found"}), 404)
+
+        #Repair candidate 
+        code_repair = request.files.get('source_code_file')
+        code_repair = code_repair.read()
+
+        #Temporarily save test code and rapair candidate
+        temp_code_path = "public/temp/" + (lambda x: x.split('/')[-1]) (challenge.code)
+        save_file(temp_code_path, 'wb',code_repair)    
+        test_code = challenge.tests_code
+        content_test_code = read_file(test_code,'rb')
+        temp_test_code_path = "public/temp/test-code.py"
+        save_file(temp_test_code_path,'wb',content_test_code)
+
+        #Check if repair candidate it is valid
+        result = valid_python_challenge(temp_code_path,temp_test_code_path, True)    
+
+        if 'Error' in result:
+            return make_response(jsonify(result), 409)
+        
+        code_challenge = challenge.code
+        code_challenge = read_file(code_challenge, 'rb')
+        
+        #Compute score of the repair solution 
+        score = nltk.edit_distance(code_challenge, code_repair)
+        
+        challenge_reponse = {'repair_objective': challenge.repair_objective, 'best_score': challenge.best_score}
+        
+        #Player is coming in future releases 
+        player = {'username': "John Doe"}
+
+        response = {'challenge': challenge_reponse, 'player': player, 'attempts': 0, 'score': score}
+
+        #Deletion of files at temp
+        try:
+            subprocess.call("rm " + temp_code_path, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+            subprocess.call("rm " + temp_test_code_path, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+        except CalledProcessError as err:
+            return {"Error": "Internal Server Error"}
+        
+        #Update best score for the challenge
+        if challenge.best_score == 0 or score < challenge.best_score:
+            challenge_dict = PythonChallenge.to_dict(challenge)
+            challenge_dict['best_score'] = score
+            
+            db.session.query(PythonChallenge).filter_by(id=id).update(dict(challenge_dict))
+            db.session.commit()
+
+        return jsonify({"repair": response})
+
 python_view = PythonViews.as_view('python_api')
 python.add_url_rule('/api/v1/python-challenges', defaults={'id': None}, view_func=python_view, methods=['GET'])
 python.add_url_rule('/api/v1/python-challenges', view_func=python_view, methods=['POST'])
 python.add_url_rule('/api/v1/python-challenges/<int:id>', view_func=python_view, methods=['GET', 'PUT'])
+python.add_url_rule('/api/v1/python-challenges/<int:id>/repair', view_func=python_view, methods=['POST'])
