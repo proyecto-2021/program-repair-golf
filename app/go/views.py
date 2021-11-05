7406b00f357d9c9ad385434f66196864b6c5ab82
EAMolina500 <molinaenzoagustin@gmail.com>
2021-11-05 04:37:57 -0300

    save old data and removing useless files. Ref #29

--- app/go/views.py
+++ app/go/views.py
@@ -38,64 +38,56 @@ def return_single_challenge(id):
 def update_a_go_challenge(id):
     challenge = GoChallenge.query.filter_by(id = id).first()
     if challenge is None:
-        return make_response(jsonify({'Challenge' : 'not found'}), 404)
+        return make_response(jsonify({'challenge' : 'not found'}), 404)
     
+    old_code_path = challenge.code
+    old_test_path = challenge.tests_code
+
     request_data = json.loads(request.form.get('challenge'))['challenge']
-
-    if 'source_code_file_name' in request_data:
-        new_code_name = request_data['source_code_file_name']
-        new_code_path = f'example-challenges/go-challenges/{new_code_name}'
-
-        #if (os.path.isfile(new_code_path) and new_code_path != challenge.code):
-        #    return make_response(jsonify({'challenge' : 'existing code file name'}), 409) 
-
-        challenge.code = new_code_path
-
-    if 'test_suite_file_name' in request_data:
-        new_test_name = request_data['test_suite_file_name']
-        new_test_path = f'example-challenges/go-challenges/{new_test_name}'
-
-        #if (os.path.isfile(new_test_path) and new_test_path != challenge.tests_code):
-        #    return make_response(jsonify({'challenge' : 'existing test suite file name'}), 409)
-
-        challenge.tests_code = new_test_path
-
-    code_path = str(challenge.code)
-    test_path = str(challenge.tests_code)
 
     change = False
     
     new_code = 'source_code_file' in request.files 
     if new_code:
-        file = request.files['source_code_file']
-        file.save(code_path)
-        code_compile = subprocess.run(["go", "build", code_path],stderr=subprocess.STDOUT, stdout=subprocess.DEVNULL)
+        if not ('source_code_file_name' in request_data):
+            return make_response(jsonify({'file name' : 'conflict'}), 409)
+
+        new_code_name = request_data['source_code_file_name']
+        new_code_path = f'example-challenges/go-challenges/{new_code_name}'  
+
+        code_compile = subprocess.run(["go", "build", new_code_path],stderr=subprocess.STDOUT, stdout=subprocess.DEVNULL)
         if code_compile.returncode == 2:
-            os.remove(code_path)
-            return make_response(jsonify({"code_file":"code with sintax errors"}),409)
+            return make_response(jsonify({"code_file":"code with sintax errors"}),409)  
+
+        file = request.files['source_code_file']
+        file.save(old_code_path)          
 
         change = True
         
-    
     new_test = 'test_suite_file' in request.files
     if new_test: 
-        file = request.files['test_suite_file']   
-        file.save(test_path)
+        if not ('test_suite_file_name' in request_data):
+            return make_response(jsonify({'file name' : 'conflict'}),409)
+        
+        new_test_name = request_data['test_suite_file_name']
+        new_test_path = f'example-challenges/go-challenges/{new_test_name}'
+
         test_compile = subprocess.run(["go", "test", "-c"],cwd='example-challenges/go-challenges')        
         if test_compile.returncode == 1:
-            os.remove(test_path)
             return make_response(jsonify({"test_code_file":"test with sintax errors"}),409)
 
+        file = request.files['test_suite_file']   
+        file.save(old_test_path)
+
         change = True
-        #challenge.tests_code = content(test_path)
 
     if change == True:
         pass_test_suite = subprocess.run(['go', 'test'], cwd='example-challenges/go-challenges')
         if pass_test_suite.returncode == 0:
             if new_code: 
-                os.remove(code_path)
+                os.remove(old_code_path)
             if new_test:
-                os.remove(test_path)
+                os.remove(old_test_path)
             return make_response(jsonify({'test_code_file':'test must fails'}), 412)
 
     if 'repair_objective' in request_data and request_data['repair_objective'] != challenge.repair_objective:
