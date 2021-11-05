80b6591551cd1db9f83eb924b2b6a9f50f6ae861
nachoo <nachogonz2702@yahoo.com.ar>
2021-11-04 21:45:23 -0300

    Added method for update and deletion of challenge #191

--- app/python/PythonChallenge.py
+++ app/python/PythonChallenge.py
@@ -1,4 +1,4 @@
-from .subprocess_utils import valid_python_challenge
+from .subprocess_utils import valid_python_challenge, delete_file
 from .file_utils import save_file
 from .PythonSourceCode import PythonSourceCode
 
@@ -19,6 +19,15 @@ class PythonChallenge:
       self.repair_objective = challenge_data['repair_objective']
       self.complexity = challenge_data['complexity']
 
+  def update(self, **new_data):
+    challenge_data = new_data.get('challenge_data')
+    if challenge_data is None: challenge_data = {}
+    #set new values
+    self.code.update(new_data.get('code'), challenge_data.get('code_name'))
+    self.test.update(new_data.get('test'), challenge_data.get('test_name'))
+    if challenge_data.get('repair') != None: self.repair_objective = challenge_data.get('repair')
+    if challenge_data.get('complexity') != None: self.complexity = challenge_data.get('complexity')
+  
   def is_valid(self):
     return valid_python_challenge(self.code.path, self.test.path)
   
@@ -28,7 +37,7 @@ class PythonChallenge:
   def test_path(self):
     return self.test.path
 
-  #saves source code ath new path
+  #saves source code at new base path
   def save_at(self, path):
     new_code_path = path + self.code.name
     save_file(new_code_path, "wb", self.code.content)
@@ -37,6 +46,11 @@ class PythonChallenge:
     new_test_path = path + self.test.name
     save_file(new_test_path, "wb", self.test.content)
     self.test.path = new_test_path
+
+  #deletes content in paths if saved somewhere
+  def delete(self):
+    delete_file(self.code.path)
+    delete_file(self.test.path)
 
   #if content is true the json will contain code, otherwise it will contain the paths of code
   def to_json(self, content = True):
