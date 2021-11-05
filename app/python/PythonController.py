aa0995875d732700d7fbd8b7616fe8b52a2489d7
nachoo <nachogonz2702@yahoo.com.ar>
2021-11-04 21:46:02 -0300

    Implemented put challenge #191

--- app/python/PythonController.py
+++ app/python/PythonController.py
@@ -38,7 +38,30 @@ class PythonController:
     #create response
     response = challenge.to_json()
     response['id'] = challenge_id
-    return {"Challenge" : response}
+    return response
+
+  def put_challenge(id, challenge_data, new_code, new_test):
+    save_to = "public/challenges/"  #general path were code will be saved
+    #retrieve challenge from db
+    req_challenge = PythonChallengeDAO.get_challenge(id)
+    if req_challenge is None:
+      return {"Error": "Challenge not found"}
+    
+    #get as Challenge object
+    original_challenge = challenge_update = PythonChallenge(challenge_data=req_challenge)
+    #create a challenge with the requested updates
+    challenge_update.update(code=new_code, test=new_test, challenge_data=challenge_data)
+    
+    validation_result = PythonController.perform_validation(challenge_update)
+    if 'Error' in validation_result:
+      return validation_result
+    #update files in system
+    original_challenge.delete()
+    challenge_update.save_at(save_to)
+    #prepare response
+    response = challenge_update.to_json()
+    response['best_score'] = req_challenge.best_score
+    return response
 
   #takes the challenge to a temp location and checks if it's valid
   @staticmethod
