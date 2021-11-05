0f327a0883b02a645565af30904f6e12175b051a
Luchit0-afk <lucianopalacio91@gmail.com>
2021-11-04 23:27:49 -0300

    Add a test who check a repair when pass a correct file. Ref #240

---
+++ tests/go/tests_rest_api.py
@@ -0,0 +1,35 @@
+import os
+import pytest
+from app import create_app, db
+from . import client
+from app.go.models_go import GoChallenge
+
+def test_repair_for_correct_file(client):
+    # arrange
+    challenge = {
+        'source_code_file': open('tests/go/files-for-tests/median.go', 'rb'),
+        'test_suite_file': open('tests/go/files-for-tests/median_test.go', 'rb'),
+        'challenge': '{ \
+                "challenge": { \
+                    "source_code_file_name" : "code", \
+                    "test_suite_file_name" : "code_test", \
+                    "repair_objective" : "repair", \
+                    "complexity" : "100", \
+                    "best_score" : 100 \
+                } \
+            }'
+    }
+
+    challenge_repair = {
+        'source_code_file': open('tests/go/files-for-tests/median_solution_3point.go', 'rb'),
+    }
+
+    #Act
+    ret_post = client.post("go/api/v1/go-challenges", data=challenge)
+    ret_post_json = ret_post.json["challenge"]
+
+    ret_repair = client.post(f"go/api/v1/go-challenges/{ret_post_json['id']}/repair", data=challenge_repair)
+
+    #Assert
+
+    assert ret_repair.status_code == 200
