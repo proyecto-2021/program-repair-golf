da87545fd9c1a4f316540e89b3dc1a4e53bd7058
Nicolas-Ortiz <nicortiz1234@gmail.com>
2021-11-05 14:29:53 -0300

    files updated correctly. Ref #29

--- app/go/views.py
+++ app/go/views.py
@@ -42,7 +42,6 @@ def update_a_go_challenge(id):
     
     old_code_path = challenge.code
     old_test_path = challenge.tests_code
-
     request_data = json.loads(request.form.get('challenge'))['challenge']
 
     change = False
@@ -75,30 +74,46 @@ def update_a_go_challenge(id):
         g = request.files['test_suite_file']   
         g.save(new_test_path)
 
-        test_compile = subprocess.run(["go", "test", "-c"],cwd='example-challenges/go-challenges/tmp')        
+        test_compile = subprocess.run(["go", "test", "-c"],cwd='example-challenges/go-challenges/tmp/')        
         if test_compile.returncode == 1:
             return make_response(jsonify({"test_code_file":"test with sintax errors"}),409)
 
         change = True
 
-    if change == True:
-        pass_test_suite = subprocess.run(['go', 'test'], cwd='example-challenges/go-challenges/tmp')
+    if change:
+        pass_test_suite = subprocess.run(['go', 'test'], cwd='example-challenges/go-challenges/tmp/')
         if pass_test_suite.returncode == 0:
-            #if new_code: 
-                #os.remove(new_code_path)
-            #if new_test:
-                #os.remove(new_test_path)
-
+            os.remove(new_code_path)
+            os.remove(new_test_path)
             return make_response(jsonify({'test_code_file':'test must fails'}), 412)
 
-        #if new_code_path:
-        h = request.files['source_code_file']
-        h.save(old_code_path)
-            #os.remove(new_code_path)
-        #if new_test_path:
-        i = request.files['test_suite_file'] 
-        i.save(old_test_path)
-            #os.remove(new_test_path)
+    if new_code:
+        with open(new_code_path) as source_code_file:
+            with open(old_code_path, 'w') as updateable_file:
+                for line in source_code_file:
+                    updateable_file.write(line)
+                
+        os.remove(new_code_path)
+
+    if new_test:
+        with open(new_test_path) as test_suite_file:
+            with open(old_test_path, 'w') as updateable_file:
+                for line in test_suite_file:
+                    updateable_file.write(line)
+        
+        os.remove(new_test_path)
+    
+    #delete all files 
+    #no es eficiente si la carpeta tmp tiene subdirectorios, pero ese caso no creo que ocurra
+    for file in os.listdir('example-challenges/go-challenges/tmp/'):
+        os.remove(os.path.join('example-challenges/go-challenges/tmp/', file))
+
+    #ver que tenemos guardado en la carpeta temporal:
+    # si solo esta source_code_file, agregar la test_suite vieja
+    # si solo esta test_suite_file, agregar source_code file viejo
+    # otro caso, cambio ambos archivos
+    #if change and (new_code and not new_test):
+
 
     if 'repair_objective' in request_data and request_data['repair_objective'] != challenge.repair_objective:
         challenge.repair_objective = request_data['repair_objective']
