6ffcf03f17d9c11d9b1f21bbc438846defa196f3
jpbn456 <jpbortol@gmail.com>
2021-11-05 19:16:15 -0300

    ref #247 get_challenge_dao excption added to the dao module

--- app/javascript/dao/challenge_dao.py
+++ app/javascript/dao/challenge_dao.py
@@ -1,10 +1,17 @@
+from app.javascript.exceptions.dao_get_challenge_exception import dao_get_challenge_exception
 from ..models_js import JavascriptChallenge, db
 
+
 def get_challenge(id):
-    return JavascriptChallenge.query.filter_by(id==JavascriptChallenge.id).first()
+    challenge = JavascriptChallenge.query.filter_by(id==JavascriptChallenge.id).first()
+    if challenge == None: 
+        raise dao_get_challenge_exception(f'the id does not exist or is null',dao_get_challenge_exception.HTTP_NOT_FOUND)  
+    return challenge
+    
 
 def get_all_challenges():
-    return JavascriptChallenge.query.all()
+    challenges = JavascriptChallenge.query.all()
+    return challenges
 
 def save_challenge(code, test_code, repair_objective,complexity,best_score):
     challenge = JavascriptChallenge(code,test_code,repair_objective,complexity,best_score)
