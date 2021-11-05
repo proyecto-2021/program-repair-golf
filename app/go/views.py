a2f9d3896536f98b37ada37315f466bb72349398
Nicolas-Ortiz <nicortiz1234@gmail.com>
2021-11-05 01:12:42 -0300

    save update files. Ref #29

--- app/go/views.py
+++ app/go/views.py
@@ -66,19 +66,24 @@ def update_a_go_challenge(id):
     change = False
     
     new_code = 'source_code_file' in request.files 
-    if new_code: 
+    if new_code:
+        file = request.files['source_code_file']
+        file.save(code_path)
         code_compile = subprocess.run(["go", "build", code_path],stderr=subprocess.STDOUT, stdout=subprocess.DEVNULL)
         if code_compile.returncode == 2:
+            os.remove(code_path)
             return make_response(jsonify({"code_file":"code with sintax errors"}),409)
 
         change = True
-        #challenge.code = content(code_path)
+        
     
     new_test = 'test_suite_file' in request.files
     if new_test: 
-                    
+        file = request.files['test_suite_file']   
+        file.save(test_path)
         test_compile = subprocess.run(["go", "test", "-c"],cwd='example-challenges/go-challenges')        
         if test_compile.returncode == 1:
+            os.remove(test_path)
             return make_response(jsonify({"test_code_file":"test with sintax errors"}),409)
 
         change = True
@@ -87,12 +92,11 @@ def update_a_go_challenge(id):
     if change == True:
         pass_test_suite = subprocess.run(['go', 'test'], cwd='example-challenges/go-challenges')
         if pass_test_suite.returncode == 0:
+            if new_code: 
+                os.remove(code_path)
+            if new_test:
+                os.remove(test_path)
             return make_response(jsonify({'test_code_file':'test must fails'}), 412)
-
-        #if new_code:
-        #    challenge.code = content(code_path)
-        #if new_test:
-        #    challenge.tests_code = content(test_path)
 
     if 'repair_objective' in request_data and request_data['repair_objective'] != challenge.repair_objective:
         challenge.repair_objective = request_data['repair_objective']
