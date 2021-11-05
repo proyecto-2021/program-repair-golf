814ed99114da8ab1363167919442d00bf4d958c7
Luchit0-afk <lucianopalacio91@gmail.com>
2021-11-05 18:33:13 -0300

    Add a test who check the upgrade in the best score. Ref #240

--- tests/go/tests_rest_api.py
+++ tests/go/tests_rest_api.py
@@ -64,7 +64,6 @@ def test_repair_for_incorrect_file(client):
 
     assert ret_repair.status_code == 409
 
-
 def test_repair_for_check_calculate_edit_distance(client):
     # arrange
     challenge = {
@@ -94,3 +93,42 @@ def test_repair_for_check_calculate_edit_distance(client):
 
     # Assert
     assert ret_repair_json["score"] == 3
+
+def test_repair_for_check_upgrade_best_score(client):
+    # arrange
+    challenge = {
+        'source_code_file': open('tests/go/files-for-tests/median.go', 'rb'),
+        'test_suite_file': open('tests/go/files-for-tests/median_test.go', 'rb'),
+        'challenge': '{ \
+                    "challenge": { \
+                        "source_code_file_name" : "code4", \
+                        "test_suite_file_name" : "code4_test", \
+                        "repair_objective" : "repair", \
+                        "complexity" : "100", \
+                        "best_score" : 100 \
+                    } \
+                }'
+    }
+
+    challenge_repair = {
+        'source_code_file': open('tests/go/files-for-tests/median_solution_3point.go', 'rb'),
+    }
+
+    # Act
+    ret_post = client.post("go/api/v1/go-challenges", data=challenge)
+    ret_post_json = ret_post.json["challenge"]
+
+    ret_repair = client.post(f"go/api/v1/go-challenges/{ret_post_json['id']}/repair", data=challenge_repair)
+    ret_repair_json = ret_repair.json["repair"]
+    score = ret_repair_json["score"]
+    ret_repair_json = ret_repair_json["challenge"]
+
+    # Assert
+    assert score == 3
+    #Testeo si el bestscore anterior es mayor al actual. Esto se debe cumplir ya que
+    #es la primera vez que reparamos el challengue.
+    assert ret_post_json["best_score"] > ret_repair_json["best_score"]
+
+
+
+
