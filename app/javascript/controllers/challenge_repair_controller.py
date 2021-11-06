5335c248e9d7dd369f937a158aa59ddd8663e269
EvelynLassalle <evelassalla@gmail.com>
2021-11-06 15:53:02 -0300

    Modification method repair Ref #219

--- app/javascript/controllers/challenge_repair_controller.py
+++ app/javascript/controllers/challenge_repair_controller.py
@@ -3,22 +3,34 @@ from flask import make_response,jsonify,request
 from controllers.files_controller import open_file, exist_file, to_temp_file, replace_file,upload_file
 from .. import db
 from ..modules.source_code_module import compile_js, test_run
+from ..exceptions import ChallengeRepairException
 
 class ChallengeRepairController():
 
-    def repair(challenge):
+    def repair(self,challenge,code_files_new):
+        file_path_new = to_temp_file(challenge.code)  
+        upload_file(code_files_new, file_path_new)
+        compile_js(file_path_new)
+        test_run(challenge.tests_code)
+
+        score = self.calculate_score(challenge)
+        if not self.score_ok(score,challenge.best_score):
+            raise ChallengeRepairException(f'The proposed score{score} is not less than the current score{challenge.best_score}', ChallengeRepairException.HTTP_NOT_FOUND)
+
+        replace_file(file_path_new, challenge.code)
+        self.update_score(challenge,score)
+
         challenge_dict = challenge.to_dict()
 
         for k in ['id','code','complexity','tests_code']:
-            del challenge_dict[k]
+             del challenge_dict[k]
 
         return make_response(jsonify( {'repair' :{
-                        'challenge': challenge_dict,
-                        'player': {'username': 'user'},
-                        'attemps': '1',
-                        'score': calculate_score
-                        }}, 200))
-
+                                'challenge': challenge_dict,
+                                'player': {'username': 'user'},
+                                'attemps': '1',
+                                'score': score
+                                }}, 200))
 
     def score_ok(score,best_score):
         return score < best_score or best_score == 0
