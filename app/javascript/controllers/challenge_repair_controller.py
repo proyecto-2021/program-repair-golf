04f2b2017b0f6f8deb1cff5a7cd6d9c52235ef84
LucianoOga <luchovh17@gmail.com>
2021-11-05 22:22:55 -0300

    Created method score_ok and ChallengeRepairException. Ref #219

--- app/javascript/controllers/challenge_repair_controller.py
+++ app/javascript/controllers/challenge_repair_controller.py
@@ -4,25 +4,30 @@ from controllers.files_controller import open_file, exist_file, to_temp_file, re
 from .. import db
 from ..modules.source_code_module import compile_js, test_run
 
-def repair(challenge):
-    challenge_dict = challenge.to_dict()
+class ChallengeRepairController():
 
-    for k in ['id','code','complexity','tests_code']:
-        del challenge_dict[k]
+    def repair(challenge):
+        challenge_dict = challenge.to_dict()
 
-    return make_response(jsonify( {'repair' :{
-                    'challenge': challenge_dict,
-                    'player': {'username': 'user'},
-                    'attemps': '1',
-                    'score': calculate_score
-                    }}, 200))
+        for k in ['id','code','complexity','tests_code']:
+            del challenge_dict[k]
+
+        return make_response(jsonify( {'repair' :{
+                        'challenge': challenge_dict,
+                        'player': {'username': 'user'},
+                        'attemps': '1',
+                        'score': calculate_score
+                        }}, 200))
 
 
-def update_score(challenge,score):
-    challenge.best_score = score
-    db.session.commit()
-    return challenge
+    def score_ok(score,best_score):
+        return score < best_score or best_score == 0
 
-def calculate_score(challenge):
-    file_path_new = to_temp_file(challenge.code) 
-    return nltk.edit_distance(open_file(challenge.code), open_file(file_path_new))
+    def update_score(challenge,score):
+        challenge.best_score = score
+        db.session.commit()
+        return challenge
+
+    def calculate_score(challenge):
+        file_path_new = to_temp_file(challenge.code) 
+        return nltk.edit_distance(open_file(challenge.code), open_file(file_path_new))
