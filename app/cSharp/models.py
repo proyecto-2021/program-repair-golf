985a3bb4d0075ccaa712fc6ca04d037a90cf2b24
iriurri <urricelquiirina@gmail.com>
2021-11-08 19:18:07 -0300

    Rename class and table. #225

--- app/cSharp/models.py
+++ app/cSharp/models.py
@@ -1,10 +1,10 @@
 from app import db
 
-class CSharp_Challenge(db.Model):
+class CSharpChallengeModel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     code = db.Column(db.String(256), unique=True, nullable=False)
     tests_code = db.Column(db.String(256), nullable=False)
-    repair_objetive = db.Column(db.String(120), nullable=False)
+    repair_objective = db.Column(db.String(120), nullable=False)
     complexity = db.Column(db.Integer, nullable=False)
     best_score = db.Column(db.Integer, nullable=False)
 
