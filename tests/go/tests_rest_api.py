1d08cfc032a4e643cacb4898252e462d636f17b2
giung1 <agus_98_jov@hotmail.com.ar>
2021-11-08 23:58:24 -0300

    New test, working in other stuffs. Ref #175

--- tests/go/tests_rest_api.py
+++ tests/go/tests_rest_api.py
@@ -4,6 +4,12 @@ from app import create_app, db
 from . import client
 from app.go.models_go import GoChallenge
 
+def add_new_challenge():
+	challenge=GoChallenge(code="tests/go/files-for-tests/median.go",tests_code="tests/go/files-for-tests/median_test.go",repair_objective="repair",complexity="coplexity",best_score=100)
+	db.session.add(challenge)
+	db.session.commit()
+	return challenge.id
+
 def test_get_all_empty(client):
     # arrange
 
@@ -13,6 +19,22 @@ def test_get_all_empty(client):
     # assert
     assert (ret_get_json=="not found")
     assert ret_get.status_code == 404
+
+def test_get_all_working(client):
+    # arrange
+    for i in range(0,3):
+        add_new_challenge()
+        
+    
+    # act
+    ret_get = client.get("/go/api/v1/go-challenges")
+    ret_get_json=ret_get.json["challenges"]
+    
+    # assert
+    assert len(ret_get_json) == 3
+    assert ret_get.status_code == 200
+
+    
 
 
 def test_getId_for_id_correct(client):
