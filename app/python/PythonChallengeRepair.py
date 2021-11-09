3c84663ad14b45df5478efbfc8dabfb15223d091
AgustinCalderon <agustincalderon058@gmail.com>
2021-11-09 12:57:49 -0300

    Updated some methods #243

--- app/python/PythonChallengeRepair.py
+++ app/python/PythonChallengeRepair.py
@@ -1,27 +1,23 @@
 from .PythonChallenge import PythonChallenge
+from nltk import edit_distance
 
 class PythonChallengeRepair:
 
-    def __init__ (self, challenge, code_repair)
+    def __init__ (self, challenge, code_repair):
         self.challenge = challenge
-        self.code_repair = PythonSourceCode(code_repair, "code_repair")
+        self.code_repair = PythonSourceCode(code = code_repair, name = self.challenge.code.name)
         
-    def is_valid_repair(temp_code_path, temp_test_code_path):
-        return valid_python_challenge(temp_code_path, temp_test_code_path, True)
+    def is_valid_repair(self): 
+        return valid_python_challenge(self.challenge.test.path, self.code_repair.code.path, True)
     
     def compute_repair_score(self):
-        code_challenge = self.challenge.code_path().read()
-        return nltk.edit_distance(code_challenge, self.code_repair)
+        return edit_distance(self.challenge.code.content, self.code_repair.code.content)
 
-    def temporary_save(path, content)
-        save_file(path, 'wb', content)
+    #Save temporary code repair and test code
+    def temporary_save(self, path):
+        #Save code repair
+        self.code_repair.move_code(path)
     
-    def save_in(challenge, path)
-        #TODO : ver como guardar el test code
-        
-        #test_code = challenge.tests_code
-        #content_test_code = read_file(test_code,'rb')
-        #save_file(path, 'wb', content_test_code)
+        #Save test code 
+        self.challenge.test.move_code(path)        
 
-    def path_temporary(path)
-        return "public/temp/" + get_filename(path)
