4245026b4ee8730f587a99d5b7bf664444d9f982
AgustinCalderon <agustincalderon058@gmail.com>
2021-11-09 00:52:00 -0300

    Defined method to saved temp #243

--- app/python/PythonChallengeRepair.py
+++ app/python/PythonChallengeRepair.py
@@ -5,7 +5,16 @@ class PythonChallengeRepair:
     def __init__ (self, challenge, code_repair)
         self.challenge = challenge
         self.code_repair = PythonSourceCode(code_repair, "code_repair")
+        
+    def temporary_save(path, content)
+        save_file(path, 'wb', content)
+    
+    def save_in(challenge, path)
+        #TODO : ver como guardar el test code
+        
+        #test_code = challenge.tests_code
+        #content_test_code = read_file(test_code,'rb')
+        #save_file(path, 'wb', content_test_code)
 
-    #def valid_repair()
-
-    #def repair_score()
+    def path_temporary(path)
+        return "public/temp/" + get_filename(path)
