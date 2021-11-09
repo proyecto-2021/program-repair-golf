ad98421ed25a2695f09c0653369424467e2265bd
Agustin Nolasco <franconolasco99@gmail.com>
2021-11-09 14:55:12 -0300

    Parametrize tests. Ref #277

--- tests/ruby/test_rubychallangedao.py
+++ tests/ruby/test_rubychallangedao.py
@@ -1,23 +1,33 @@
+import random
+import string
+import pytest
 from . import client
 from app.ruby.rubychallengedao import RubyChallengeDAO
 
-def test_create_challenge(client):
+def get_tests_data(n):
+    tests_data_list = []
+    for _ in range(n):
+        data = dict()
+        data['code'] = str(random.choices(string.ascii_letters + string.digits,k=10))
+        data['tests_code'] = str(random.choices(string.ascii_letters + string.digits,k=10))
+        data['repair_objective'] = str(random.choices(string.ascii_letters + string.digits,k=10))
+        data['complexity'] = str(random.randint(1,5))
+        tests_data_list.append(data)
+    return tests_data_list
+
+@pytest.mark.parametrize("data", get_tests_data(5))
+def test_create_challenge(client, data):
     dao = RubyChallengeDAO()
-    id = dao.create_challenge('path/to/example.rb', 'path/to/example_tests.rb', 'Testing', '5')
+    id = dao.create_challenge(**data)
 
     assert id is not None
 
-def test_get_one_after_create(client):
+@pytest.mark.parametrize("data", get_tests_data(5))
+def test_get_one_after_create(client, data):
     dao = RubyChallengeDAO()
-    id = dao.create_challenge('path/to/example.rb', 'path/to/example_tests.rb', 'Testing', '5')
+    id = dao.create_challenge(**data)
 
     challenge = dao.get_challenge(id)
+    del challenge['best_score']
 
-
-    assert challenge == {
-        'code': 'path/to/example.rb',
-        'tests_code': 'path/to/example_tests.rb',
-        'repair_objective': 'Testing',
-        'best_score':  0,
-        'complexity': '5'
-    }
+    assert challenge == data
