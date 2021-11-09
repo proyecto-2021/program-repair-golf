0920ec3ad02b7bfb48e9189a4dde0cf1a9b0b16b
AgustinCalderon <agustincalderon058@gmail.com>
2021-11-08 21:01:03 -0300

    Create new class #243

---
+++ app/python/PythonChallengeRepair.py
@@ -0,0 +1,14 @@
+from .PythonChallenge import PythonChallenge
+
+class PythonChallengeRepair:
+
+    def __init__ (self, challenge, code_repair)
+        self.challenge = challenge
+        self.code_repair = PythonSourceCode(code_repair, "code_repair")
+
+    #def valid_repair()
+        
+
+    #def repair_scrore()
+    
+    
