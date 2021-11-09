c69a60a2f3e1e2a70ca614e27ff803813856372a
ritazabala <82459669+ritazabala@users.noreply.github.com>
2021-11-09 17:13:11 -0300

    Update test_basics_db.py

--- tests/java/test_basics_db.py
+++ tests/java/test_basics_db.py
@@ -16,34 +16,6 @@ def create_dict(code, test, repair, complexity):
         }
     return dict
 
-#def test_new_challenge(client):
-def test_ViewAllChallenges(client):
-    challenge = {"challenges":[]}
-    challenge ['challenges'] = DAO_java_challenge.all_challenges_java()
-    resp = len(challenge)
-    assert resp == 1
-
-    #verrrrrrrrrrrrrrrrrrrrrrrrrrrr 
-    '''
-    dict={"dict":[]}
-    dict['source_code_file_name']='source_code_file_name'
-    dict['test_suite_file_name']='test_suite_file_name'
-    dict['repair_objective']='repair_objective'
-    dict['complexity']='complexity'
-    dict['score']=500
-    DAO_java_challenge.create_challenge(dict)
-    resp = len(challenge)
-    challenge ['challenges'] = DAO_java_challenge.all_challenges_java()
-    assert resp == 2
-    '''
-
-def test_ViewChallenges(client):
-    challenge = {"challenges":[]}
-    id=1
-    challenge ['challenges'] = DAO_java_challenge.challenges_id_java(id)
-    resp = len(challenge)
-    assert resp == 1 
-
 def test_get_not_exist_id(client):
     code1 = "code1"
     test1 = "test1"
