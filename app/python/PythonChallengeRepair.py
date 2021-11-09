3bad88b0cfdefbd1a88245061cbf9287dc197dcf
AgustinCalderon <agustincalderon058@gmail.com>
2021-11-09 00:54:34 -0300

    Defined methods valid repair and compute score #243

--- app/python/PythonChallengeRepair.py
+++ app/python/PythonChallengeRepair.py
@@ -6,6 +6,13 @@ class PythonChallengeRepair:
         self.challenge = challenge
         self.code_repair = PythonSourceCode(code_repair, "code_repair")
         
+    def is_valid_repair(temp_code_path, temp_test_code_path):
+        return valid_python_challenge(temp_code_path, temp_test_code_path, True)
+    
+    def compute_repair_score(self):
+        code_challenge = self.challenge.code_path().read()
+        return nltk.edit_distance(code_challenge, self.code_repair)
+
     def temporary_save(path, content)
         save_file(path, 'wb', content)
     
