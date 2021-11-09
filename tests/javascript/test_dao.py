d18d97f558d7224905a11b534d3d4d4fa7838816
EvelynLassalle <evelassalla@gmail.com>
2021-11-09 17:50:27 -0300

    was added test get challenge 1 to test_dao Ref #147

--- tests/javascript/test_dao.py
+++ tests/javascript/test_dao.py
@@ -7,3 +7,18 @@ from app.javascript.dao.challenge_dao import ChallengeDAO
 def test_get_all_challenges(client):
     empty_list = ChallengeDAO.get_all_challenges()
     assert empty_list == []   
+
+def test_get_challenge_1(client):
+    challenge = JavascriptChallenge(
+        code = "empty code",
+        tests_code = "empty test",
+        repair_objective = "something",
+        complexity = 0,
+        best_score = 0,
+    )
+    
+    db.session.add(challenge)
+    db.session.commit()
+
+    challenge_2 = ChallengeDAO.get_challenge(1)
+    assert challenge == challenge_2
