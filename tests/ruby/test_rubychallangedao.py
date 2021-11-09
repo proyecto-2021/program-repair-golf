b139272d3bdca4a12051528113865d0e04a083c2
Agustin Nolasco <franconolasco99@gmail.com>
2021-11-09 11:09:49 -0300

    Add new tests, after create challenge if a get the challenge by id then is the same. Ref #277

--- tests/ruby/test_rubychallangedao.py
+++ tests/ruby/test_rubychallangedao.py
@@ -3,6 +3,21 @@ from app.ruby.rubychallengedao import RubyChallengeDAO
 
 def test_create_challenge(client):
     dao = RubyChallengeDAO()
-    id = dao.create_challenge("path/to/example.rb","path/to/example_tests.rb","Testing","5")
+    id = dao.create_challenge('path/to/example.rb', 'path/to/example_tests.rb', 'Testing', '5')
 
     assert id is not None
+
+def test_get_one_after_create(client):
+    dao = RubyChallengeDAO()
+    id = dao.create_challenge('path/to/example.rb', 'path/to/example_tests.rb', 'Testing', '5')
+
+    challenge = dao.get_challenge(id)
+
+
+    assert challenge == {
+        'code': 'path/to/example.rb',
+        'tests_code': 'path/to/example_tests.rb',
+        'repair_objective': 'Testing',
+        'best_score':  0,
+        'complexity': '5'
+    }
