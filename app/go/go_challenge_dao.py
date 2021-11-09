9bd21a890916d34251c25a21ace548c07a7f1ad7
EAMolina500 <molinaenzoagustin@gmail.com>
2021-11-09 18:48:57 -0300

    add intance in the methods Ref #264

--- app/go/go_challenge_dao.py
+++ app/go/go_challenge_dao.py
@@ -3,25 +3,26 @@ from . import go
 from .models_go import GoChallenge
 
 class goChallengeDAO():
-	def get_all_challenges():
+	def get_all_challenges(self):
 		return db.session.query(GoChallenge).all()
 
-	def get_challenge_by_id(id):
+	def get_challenge_by_id(self, id):
 		return GoChallenge.query.filter_by(id=id).first()
 
-	def create_challenge(challenge):
+	def create_challenge(self, code, tests_code, repair_objective, complexity, best_score):
 		new_challenge = GoChallenge(
-			code = challenge.code,
-			tests_code = challenge.tests_code,
-			repair_objective = challenge.repair_objective,
-			complexity = challenge.complexity,
-			best_score = 0)
+			code = code,
+			tests_code = tests_code,
+			repair_objective = repair_objective,
+			complexity = complexity,
+			best_score = 0
+			)
 
 		db.session.add(new_challenge)
 		db.session.commit()
 
 		return new_challenge.id
 
-	def update_challenge(id, challenge):
+	def update_challenge(self, id, challenge):
 		db.session.query(GoChallenge).filter_by(id=id).update(challenge)
 		db.session.commit()
