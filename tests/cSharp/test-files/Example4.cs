45e5b3f7fbdc20226196552150076f9aed657686
RubenDanielle <daniellerubenr@gmail.com>
2021-11-10 19:41:19 -0300

    Add new files for use in testing. #295

---
+++ tests/cSharp/test-files/Example4.cs
@@ -0,0 +1,11 @@
+// This Example is used to test post response when no test fails
+using System;
+
+public class Example4 {
+    public static string example4() {
+        return "I'm a test";
+    }
+    public static void Main(string[] args) {
+        Console.WriteLine (example4());
+    }
+}
