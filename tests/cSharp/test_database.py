b2788ce848b4e5ac8e6c0fe7e5c0abe7cf2fef39
RubenDanielle <daniellerubenr@gmail.com>
2021-11-10 13:44:57 -0300

    Add fixtures. #286

--- tests/cSharp/test_database.py
+++ tests/cSharp/test_database.py
@@ -5,16 +5,25 @@ from app.cSharp.views import *
 import pytest
 
 
-def test_get_challenge_from_db_without_files_contents(client):
-    # Test get_challenge_db method with show_files_content=False
-    challenge = CSharpChallengeModel(code="test-code-path",
-                                     tests_code="test-test-path",
+@pytest.fixture
+def new_challenge():
+    challenge = CSharpChallengeModel(code="./tests/test-files/Example1.cs",
+                                     tests_code="./tests/test-files/Example1Test.cs",
                                      repair_objective="testing the db",
                                      complexity=5,
                                      best_score=0)
     db.session.add(challenge)
     db.session.commit()
-    ch_id = challenge.id
-    ch_dict = challenge.__repr__()
+    return challenge
+
+
+@pytest.fixture
+def expected_challenge(new_challenge):
+    return new_challenge.__repr__()
+
+
+def test_get_challenge_from_db_without_files_contents(client, expected_challenge):
+    # Test get_challenge_db method with show_files_content=False
+    ch_id = expected_challenge['id']
     challenge_from_get = get_challenge_db(ch_id)
-    assert challenge_from_get == ch_dict
+    assert challenge_from_get == expected_challenge
