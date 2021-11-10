03501f7c5c92d9bae6ac9dd2bd76ba6c0edd2187
EAMolina500 <molinaenzoagustin@gmail.com>
2021-11-09 23:10:52 -0300

    add init, delete and exists methods .Ref #264

--- app/go/go_challenge_dao.py
+++ app/go/go_challenge_dao.py
@@ -3,18 +3,22 @@ from . import go
 from .models_go import GoChallenge
 
 class goChallengeDAO():
+	def __init__(self):
+		pass 
+
 	def get_all_challenges(self):
 		return db.session.query(GoChallenge).all()
 
 	def get_challenge_by_id(self, id):
-		return GoChallenge.query.filter_by(id=id).first()
+		#return GoChallenge.query.filter_by(id=id).first()
+		return db.session.query(GoChallenge).filter_by(id=id).first()
 
-	def create_challenge(self, code, tests_code, repair_objective, complexity, best_score):
+	def create_challenge(self, challenge):
 		new_challenge = GoChallenge(
-			code = code,
-			tests_code = tests_code,
-			repair_objective = repair_objective,
-			complexity = complexity,
+			code = challenge.code,
+			tests_code = challenge.tests_code,
+			repair_objective = challenge.repair_objective,
+			complexity = challenge.complexity,
 			best_score = 0
 			)
 
@@ -26,3 +30,10 @@ class goChallengeDAO():
 	def update_challenge(self, id, challenge):
 		db.session.query(GoChallenge).filter_by(id=id).update(challenge)
 		db.session.commit()
+
+	def delete_challenge(self, challenge):
+		db.session.delete(challenge)
+		db.session.commit()
+
+	def exists(self, id):
+		return get_challenge_by_id(id) is not Nones
