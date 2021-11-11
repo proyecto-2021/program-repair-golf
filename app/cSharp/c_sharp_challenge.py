f52147d48c9930cf31ae59f345193f330936d73a
Lucrecia Mascanfroni <lucreciamascanfroni@MacBook-Air-de-Lucrecia.local>
2021-11-11 19:21:51 -0300

    Add class and init. #226

---
+++ app/cSharp/c_sharp_challenge.py
@@ -0,0 +1,7 @@
+from c_sharp_src import CSharpSrc
+
+class CSharpChallenge:
+
+	def __init__(self, code, test, code_name, test_name):
+		self.code = CSharpSrc(code, code_name)
+		self.test = CSharpSrc(test, test_name)
