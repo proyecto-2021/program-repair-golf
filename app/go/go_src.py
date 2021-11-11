26bbb0e2211c0c24f33b3c571e4ef15c3df4e1eb
EAMolina500 <molinaenzoagustin@gmail.com>
2021-11-11 11:59:13 -0300

    add save and remove and solve bugs. Ref #269

--- app/go/go_src.py
+++ app/go/go_src.py
@@ -2,29 +2,37 @@ import subprocess
 
 class Go_src:
 
-    def __init__(self, path, file_name, path_test):
+    def __init__(self, path, file=None):
         self.path = path
-        self.name_file = file_name
-        self.path_test = path_test
 
     def get_path(self):
         return self.path
 
-    def get_file_name(self):
-        return self.file_name
+    #def get_file_name(self):
+    #    return self.file_name
 
-    def get_path_test(self):
-        return self.path_test
+    #def get_path_test(self):
+    #    return self.path_test
 
     def code_compiles(self):
-        return subprocess.run(["go", "build", self.path], stderr=subprocess.STDOUT, stdout=subprocess.DEVNULL)
+        return subprocess.run(["go", "build", self.get_path()], stderr=subprocess.STDOUT, stdout=subprocess.DEVNULL)
 
     def test_compiles(self):
-        return subprocess.run(["go", "test", "-c"], cwd=self.path_test)
+        return subprocess.run(["go", "test", "-c"], cwd=self.get_path())
 
     def test_run(self):
-        return subprocess.run(["go", "test"], cwd=self.path_test)
+        return subprocess.run(["go", "test"], cwd=self.get_path())
 
-    def content(self):
-        f = open(self.path,'r')
+    def get_content(self):
+        f = open(self.get_path(),'r')
         return f.read()
+
+    def save(self):
+        self.file.save(self.get_path())
+
+    def remove(self):
+        os.remove(get_path())
+
+    #def write(self):
+    #    ...
+
