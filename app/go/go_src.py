aa66a878b73df2e8dc1aa7878b92f2c5e2bc81c6
Alan <alan_arce97@hotmail.com>
2021-11-11 10:06:29 -0300

    Add the gets and function content. Ref #269

--- app/go/go_src.py
+++ app/go/go_src.py
@@ -6,7 +6,15 @@ class Go_src:
         self.path = path
         self.name_file = file_name
         self.path_test = path_test
-        #self.content_file = content_file
+
+    def get_path(self):
+        return self.path
+
+    def get_file_name(self):
+        return self.file_name
+
+    def get_path_test(self):
+        return self.path_test
 
     def code_compiles(self):
         return subprocess.run(["go", "build", self.path], stderr=subprocess.STDOUT, stdout=subprocess.DEVNULL)
@@ -16,3 +24,7 @@ class Go_src:
 
     def test_run(self):
         return subprocess.run(["go", "test"], cwd=self.path_test)
+
+    def content(self):
+        f = open(self.path,'r')
+        return f.read()
