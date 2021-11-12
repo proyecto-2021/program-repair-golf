df6ff76464f0fafa61c276c7ff1505ad7754e72c
Ayelen Garcia <garciiaayelen@gmail.com>
2021-11-12 16:38:55 -0300

    Add method exist. #231

--- app/cSharp/c_sharp_challenge_DAO.py
+++ app/cSharp/c_sharp_challenge_DAO.py
@@ -6,3 +6,5 @@ class CSharpChallengeDAO:
     def __init__(self):
         pass
 
+    def exist(self, id):
+        return db.session.query(CSharpChallengeModel).filter_by(id=id).first() is not None
