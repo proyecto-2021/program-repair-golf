5d9f028b5ba93cafd84a0dafb3efba8c45f2d86d
iriurri <urricelquiirina@gmail.com>
2021-11-12 15:25:01 -0300

    Add test_update_code_w_sintax_error definition. #294

--- tests/cSharp/test_rest_api.py
+++ tests/cSharp/test_rest_api.py
@@ -59,6 +59,11 @@ def test_update_incorrect_complexity(client, create_test_data):
     cleanup()
 
 
+def test_update_code_w_sintax_error(client):
+    #to do: implement this method
+    pass
+
+
 def test_get_all_challenges_after_post(client, create_test_data):
     #Arrange
     url = 'cSharp/c-sharp-challenges'
