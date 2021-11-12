7dbeb528ac35c7e386c98024371b13b96ec78c99
Lucrecia Mascanfroni <lucreciamascanfroni@MacBook-Air-de-Lucrecia.local>
2021-11-12 16:48:36 -0300

    Add methods definition. #226

--- app/cSharp/c_sharp_challenge.py
+++ app/cSharp/c_sharp_challenge.py
@@ -5,3 +5,15 @@ class CSharpChallenge:
 	def __init__(self, code, test, code_name, test_name):
 		self.code = CSharpSrc(code, code_name)
 		self.test = CSharpSrc(test, test_name)
+
+	def get_code():
+		#TODO
+		pass
+
+	def get_test():
+		#TODO
+		pass
+
+	def validate():
+		#TODO
+		pass
