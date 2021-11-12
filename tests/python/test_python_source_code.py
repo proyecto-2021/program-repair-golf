624ac8c4767b93b6c216ca83d228f406c2196093
matiasg99 <matiias.gomeez999@gmail.com>
2021-11-12 19:36:24 -0300

    test to builder pythonSourceCode #302

--- tests/python/test_python_source_code.py
+++ tests/python/test_python_source_code.py
@@ -1,8 +1,18 @@
 from app.python.file_utils import *
+from app.python.PythonSourceCode import *
+from . import client
+import json
 
-
-
-
-# testing of one post challenge
-# def test_init(client):
+# testing builder 
+def test_pythonsourcecode_builder_if(client): 
+    codeToRead = read_file('tests/python/example_programs_test/valid_code_1.py','rb')
     
+    pythonSourceCodeInstance = PythonSourceCode(code = codeToRead ,name='matias');
+    
+    codeToCompare = read_file('tests/python/example_programs_test/valid_code_1.py','r')
+
+    fileReadResult = pythonSourceCodeInstance.get_content()
+    
+    assert fileReadResult == codeToCompare
+    assert pythonSourceCodeInstance.name == 'matias'
+    assert pythonSourceCodeInstance.content == codeToRead
