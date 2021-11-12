6fbc9b3e9a613a71bbe897eb33752ac011111be7
Nicolas-Ortiz <nicortiz1234@gmail.com>
2021-11-12 17:45:19 -0300

    modify implementation. Ref #265

--- app/go/go_challenge.py
+++ app/go/go_challenge.py
@@ -1,50 +1,42 @@
-from .go.src_code import Go_src
+from go_src import Go_src
 
 class GoChallenge:
 
     def __init__(self, path_code, path_tests_code, repair_objective, complexity):
-        self.code = path_code
-        self.tests_code = path_tests_code
+        self.code = Go_src(path = path_code)
+        self.tests_code = Go_src(path = path_tests_code)
         self.repair_objective = repair_objective
         self.complexity = complexity
         self.best_score = 0
 
-    #Dependiendo lo que necesitemos, deberiamos tener 2 creo.
-    #Uno que retorne el path y otro el contenido
     def get_code(self):
-        return self.code
+        return self.code.get_path()
 
     def get_code_content(self):
-        self.code = Go_src(path=path_code)
         return self.code.get_content()
 
-    #Dependiendo lo que necesitemos, deberiamos tener 2 creo.
-    #Uno que retorne el path y otro el contenido
     def get_tests_code(self):
-        return self.tests_code
+        return self.tests_code.get_path()
 
     def get_tests_content(self):
-        self.tests_code = Go_src(path=path_tests_code)
-        return self.code.get_content()
+        return self.tests_code.get_content()
 
     def get_best_score(self):
         return self.best_score
 
     #Code y tests_code tienen que ser el codigo. Usar metodo de src_code
-    def get_content(self):
+    def get_content_by_id(self):
         return {
-            self.code,
-            self.tests_code,
-            self.repair_objective,
-            self.complexity,
-            self.best_score,
+            'code': self.code.get_content(),
+            'tests_code': self.tests_code.get_content(),
+            'repair_objective':self.repair_objective,
+            'complexity':self.complexity,
+            'best_score':self.best_score,
         }
 
-    #Se le debe asignar un src_code
     def set_code(self, path_code):
         self.code = path_code
 
-    #Se le debe asignar un src_code            
     def set_tets_code(self, path_tests_code):
         self.tests_code = path_tests_code
 
@@ -54,11 +46,18 @@ class GoChallenge:
     def set_complexity(self, complexity):
         self.complexity = complexity
 
-    def codes_compiles():
+    def codes_compiles(self):
         return self.code.code_compiles() and self.tests_code.test_compiles()
 
-    def code_compiles():
+    def code_compiles(self):
         return self.code.code_compiles()
 
-    def tests_compiles():
-        return self.code.test_compiles()
+    def tests_compiles(self):
+        return self.tests_code.test_compiles()
+
+path_code = Go_src(path='example-challenges/go-challenges/median.go')
+path_test = Go_src(path='example-challenges/go-challenges/median_test.go')
+go_challenge = GoChallenge(path_code.get_path(), path_test.get_path(), 'Make asd pass.', '2')
+
+print(go_challenge.code_compiles())
+
