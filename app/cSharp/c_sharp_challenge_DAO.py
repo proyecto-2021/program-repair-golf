93b349c58f7fe4535f3390293243a6e87f422bec
Ayelen Garcia <garciiaayelen@gmail.com>
2021-11-13 20:54:23 -0300

    Add method get_challenge_db. #231

--- app/cSharp/c_sharp_challenge_DAO.py
+++ app/cSharp/c_sharp_challenge_DAO.py
@@ -6,5 +6,13 @@ class CSharpChallengeDAO:
     def __init__(self):
         pass
 
+    def get_challenge_db(self, id, show_files_content=False):
+        challenge = db.session.query(CSharpChallengeModel).filter_by(id=id).first().__repr__()
+        print(challenge)
+        if show_files_content:
+            challenge['code'] = open(challenge['code'], "r").read()
+            challenge['tests_code'] = open(challenge['tests_code'], "r").read() 
+        return challenge 
+        
     def exist(self, id):
         return db.session.query(CSharpChallengeModel).filter_by(id=id).first() is not None
