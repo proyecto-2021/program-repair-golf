8cc1ce1d80b9837f22a65283882525b9471df2e6
Ayelen Garcia <garciiaayelen@gmail.com>
2021-11-13 20:56:28 -0300

    Add method save_challenge. #231

--- app/cSharp/c_sharp_challenge_DAO.py
+++ app/cSharp/c_sharp_challenge_DAO.py
@@ -13,6 +13,12 @@ class CSharpChallengeDAO:
             challenge['code'] = open(challenge['code'], "r").read()
             challenge['tests_code'] = open(challenge['tests_code'], "r").read() 
         return challenge 
-        
+
     def exist(self, id):
         return db.session.query(CSharpChallengeModel).filter_by(id=id).first() is not None
+
+    def save_challenge(self, challenge_data, source_code_path, test_path):
+        new_challenge = CSharpChallengeModel(code = source_code_path, tests_code = test_path, repair_objective = challenge_data['repair_objective'], complexity = int(challenge_data['complexity']), best_score = 0)
+        db.session.add(new_challenge)
+        db.session.commit()
+        return new_challenge.id
