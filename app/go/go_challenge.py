2515a828e4cc85da22db94369034d02275f0ac1f
EAMolina500 <molinaenzoagustin@gmail.com>
2021-11-13 17:22:03 -0300

    add some getters and setters, solve bugs and others. Ref #332

--- app/go/go_challenge.py
+++ app/go/go_challenge.py
@@ -2,12 +2,16 @@ from go_src import Go_src
 
 class GoChallenge:
 
-    def __init__(self, path_code, path_tests_code, repair_objective, complexity):
+    def __init__(self, id=None, path_code=None, path_tests_code=None, repair_objective=None, complexity=None):
+        self.id = id
         self.code = Go_src(path = path_code)
         self.tests_code = Go_src(path = path_tests_code)
         self.repair_objective = repair_objective
         self.complexity = complexity
         self.best_score = 0
+
+    def get_id(self):
+        return self.id
 
     def get_code(self):
         return self.code.get_path()
@@ -18,26 +22,58 @@ class GoChallenge:
     def get_tests_code(self):
         return self.tests_code.get_path()
 
-    def get_tests_content(self):
+    def get_tests_code_content(self):
         return self.tests_code.get_content()
+
+    def get_repair_objective(self):
+        return self.repair_objective
+
+    def get_complexity(self):
+        return self.complexity
 
     def get_best_score(self):
         return self.best_score
 
     #Code y tests_code tienen que ser el codigo. Usar metodo de src_code
-    def get_content_by_id(self):
+    def get_content_by_id_and_put(self):
         return {
             'code': self.code.get_content(),
             'tests_code': self.tests_code.get_content(),
-            'repair_objective':self.repair_objective,
-            'complexity':self.complexity,
-            'best_score':self.best_score,
+            'repair_objective': self.repair_objective,
+            'complexity': self.complexity,
+            'best_score': self.best_score
         }
+
+    def get_content_all(self):
+        return {
+            'id': self.id,
+            'code': self.code.get_content(),
+            'repair_objective': self.repair_objective,
+            'complexity': self.complexity,
+            'best_score': self.best_score
+        }
+
+    def get_content_post(self):
+        return {
+            'id': self.id,
+            'code': self.code.get_content(),
+            'tests_code': self.tests_code.get_content(),
+            'repair_objective': self.repair_objective,
+            'complexity': self.complexity,
+            'best_score': self.best_score
+        }
+
+    def get_content_repair(self):
+        return {
+            'repair_objective': self.repair_objective,
+            'best_score': self.best_score
+        }
+
 
     def set_code(self, path_code):
         self.code.set_path(path_code)
 
-    def set_tets_code(self, path_tests_code):
+    def set_tests_code(self, path_tests_code):
         self.tests_code.set_path(path_tests_code)
 
     def set_repair_objective(self, repair_objective):
@@ -45,6 +81,9 @@ class GoChallenge:
 
     def set_complexity(self, complexity):
         self.complexity = complexity
+
+    def set_best_score(self, best_score):
+        self.best_score = best_score
 
     def codes_compiles(self):
         return self.code.code_compiles() and self.tests_code.test_compiles()
@@ -55,11 +94,14 @@ class GoChallenge:
     def tests_compiles(self):
         return self.tests_code.test_compiles()
 
+
+
 path_code = Go_src(path='example-challenges/go-challenges/median.go')
 path_test = Go_src(path='example-challenges/go-challenges/median_test.go')
-go_challenge = GoChallenge(path_code.get_path(), path_test.get_path(), 'Make asd pass.', '2')
+go_challenge = GoChallenge(id=5, path_code=path_code.get_path(), path_tests_code=path_test.get_path(), repair_objective='Make asd pass.', complexity='2')
 
-go_challenge.set_code('app/go/views.py')
-print(go_challenge.get_code())
-print(go_challenge.get_tests_code())
+#go_challenge.set_code('app/go/views.py')
+print(go_challenge.set_best_score(5))
+print(go_challenge.get_best_score())
+
 
--- app/go/go_src.py
+++ app/go/go_src.py
@@ -2,10 +2,10 @@ import subprocess, os
 
 class Go_src:
 
-    def __init__(self, path=None, file=None, file_2=None):
+    def __init__(self, path=None, file=None):
         self.path = path
         self.file = file
-        self.file_2 = file_2
+        #self.file_2 = file_2
 
     def get_path(self):
         return self.path
@@ -13,17 +13,23 @@ class Go_src:
     def get_file(self):
         return self.file
 
-    def get_file_2(self):
-        return self.file_2
+    #def get_file_2(self):
+    #    return self.file_2
 
-    def set_path(self, new_path):
-        self.path = new_path
+    def set_path(self, path):
+        self.path = path
 
     def code_compiles(self):
-        return (subprocess.run(["go", "build", self.get_path()], stderr=subprocess.STDOUT, stdout=subprocess.DEVNULL)).returncode == 0
+        if (subprocess.run(["go", "build", self.get_path()], stderr=subprocess.STDOUT, stdout=subprocess.DEVNULL)).returncode == 0:
+            print('compilo')
+        else:
+            print('no compilo')
 
     def test_compiles(self):
-        return (subprocess.run(["go", "test", "-c"], cwd=self.get_path())).returncode == 0
+        if subprocess.run(["go test -c"],cwd=self.get_path(),shell=True) == 0:
+            print('no compilo')
+        else:
+            print('compilo')    
 
     def test_run(self):
         return (subprocess.run(["go", "test"], cwd=self.get_path())).returncode == 0
@@ -31,28 +37,44 @@ class Go_src:
     def remove_file(self):
         return subprocess.run(["rm" "-r" "solution"],cwd=os.path.abspath(self.get_path),stderr=subprocess.STDOUT, stdout=subprocess.DEVNULL, shell=True)
 
-    def generic_compile(self, command, path):
-        return subprocess.run(command, cwd=os.path.abspath(self.get_path()),stderr=subprocess.STDOUT, stdout=subprocess.DEVNULL, shell=True)
+    #def generic_compile(self, path, type):
+    #    if type == Type.CODE_COMPILE:
+    #        commands = ["go", "build"]
+    #    elif type == Type.TESTS_COMPILE:
+    #        commands = ["go", "test", "-c"]
+    #    elif type == Type.TESTS_RUN:
+    #        commands = ["go", "test"]
+    #    elif type == Type.REMOVE_FILE:
+    #        commands = ["rm" "-r" "solution"]
+
+        return subprocess.run(commands, cwd=os.path.abspath(self.get_path()), stderr=subprocess.STDOUT, stdout=subprocess.DEVNULL, shell=True).returncode == 0
 
     def get_content(self):
         with open(self.get_path(),'r') as f:
             return f.read()
 
-    def save(self):
+    def save(self):    
         self.file.save(self.get_path())
 
     def remove(self):
         os.remove(self.get_path())
 
     def write_file(self, file_update):
-        with open(self.get_file()) as f:
+        with open(self.get_path()) as f:
             with open(file_update, 'w') as file_to_update:
                 for line in f:
                     file_to_update.write(line)
 
-    def write_file_2(self):
-        with open(self.get_file()) as f:
-            with open(self.get_file_2()) as g:
-                for line in f:
-                    g.write(line) 
+    #def write_file_2(self):
+    #    with open(self.get_file()) as f:
+    #        with open(self.get_file_2()) as g:
+    #            for line in f:
+    #                g.write(line) 
 
+path_code = Go_src(path='example-challenges/go-challenges/median.go')
+path_test = Go_src(path='example-challenges/go-challenges/median_test.go')
+
+#path_code.write_file('example-challenges/go-pruebas/aaa_test.go')
+#path_test.remove()
+path_code.code_compiles()
+path_test.test_compiles()
