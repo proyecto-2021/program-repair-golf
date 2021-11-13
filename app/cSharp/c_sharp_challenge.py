cc0551604c991f462998aefa32a03ff534c1f73e
Lucrecia Mascanfroni <lucreciamascanfroni@MacBook-Air-de-Lucrecia.local>
2021-11-13 18:29:09 -0300

    Modify init. Add validate method. #226

--- app/cSharp/c_sharp_challenge.py
+++ app/cSharp/c_sharp_challenge.py
@@ -1,11 +1,25 @@
-from c_sharp_src import CSharpSrc
+from .c_sharp_src import CSharpSrc
 
 class CSharpChallenge:
 
-	def __init__(self, code, test, code_name, test_name):
-		self.code = CSharpSrc(code, code_name)
-		self.test = CSharpSrc(test, test_name)
+    def __init__(self, code, test, code_name, test_name, code_path=None, test_path=None):
+        if code_path is not None:
+            self.code = CSharpSrc(code, code_name, code_path)
+        else:
+            self.code = CSharpSrc(code, code_name)
+        if test_path is not None:
+            self.test = CSharpSrc(test, test_name, test_path)
+        else:
+            self.test = CSharpSrc(test, test_name)
 
-	def validate():
-		#TODO
-		pass
+    def validate(self):
+        if self.code.compiles():
+            if self.code.test_compiles(self.test):
+                if self.code.tests_pass(self.test):
+                    return 0
+                else:
+                    return 1
+            else:
+                return 2
+        else:
+            return -1
