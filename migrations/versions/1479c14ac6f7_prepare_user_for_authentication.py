53f07db5549d8dcbfbc2494dde499f774dfe6399
Pablo Ponzio <pponzio@dc.exa.unrc.edu.ar>
2021-11-12 22:09:08 -0300

    Make space for larger password hashes in DB migration

--- migrations/versions/1479c14ac6f7_prepare_user_for_authentication.py
+++ migrations/versions/1479c14ac6f7_prepare_user_for_authentication.py
@@ -21,7 +21,7 @@ def upgrade():
     op.create_table('user',
     sa.Column('id', sa.Integer(), nullable=False),
     sa.Column('username', sa.String(length=128), nullable=False),
-    sa.Column('password', sa.String(length=128), nullable=False),
+    sa.Column('password', sa.String(length=256), nullable=False),
     sa.PrimaryKeyConstraint('id')
     )
 
