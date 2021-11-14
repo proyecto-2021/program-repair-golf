301bd5778e26172ea072753cc72b9409111641a0
Lucrecia Mascanfroni <lucreciamascanfroni@MacBook-Air-de-Lucrecia.local>
2021-11-14 18:34:39 -0300

    Change init. #226

--- app/cSharp/c_sharp_challenge.py
+++ app/cSharp/c_sharp_challenge.py
@@ -2,15 +2,11 @@ from .c_sharp_src import CSharpSrc
 
 class CSharpChallenge:
 
-    def __init__(self, code, test, code_name, test_name, code_path=None, test_path=None):
-        if code_path is not None:
-            self.code = CSharpSrc(code, code_name, code_path)
-        else:
-            self.code = CSharpSrc(code, code_name)
-        if test_path is not None:
-            self.test = CSharpSrc(test, test_name, test_path)
-        else:
-            self.test = CSharpSrc(test, test_name)
+    def __init__(self, code, test, code_name, test_name, code_path, test_path):
+        self.code = CSharpSrc(code, code_name, code_path)
+        self.code.save()
+        self.test = CSharpSrc(test, test_name, test_path)
+        self.test.save()
 
     def validate(self):
         if self.code.compiles():
