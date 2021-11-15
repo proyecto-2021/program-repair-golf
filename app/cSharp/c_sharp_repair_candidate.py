e2ca75438acb80d821f22a801039e631511533f2
tomas98Fer <tomas016fernandez@gmail.com>
2021-11-15 19:19:50 -0300

    Add implements of method, this are not ready. #230

--- app/cSharp/c_sharp_repair_candidate.py
+++ app/cSharp/c_sharp_repair_candidate.py
@@ -3,18 +3,22 @@ from .c_sharp_challenge import cSharpChallenge
 
 class cSharpRepairCandidate:
 
- 	def __init__():
- 	 #method to implement
+ 	def __init__(self,challenge,code_file,file_name,path):
+ 		if isinstance(cSharpChallenge,challenge):
+ 			self.challenge = challenge
+ 			self.code = CSharpSrc(code_file,file_name,path) 
+ 	
  	pass
 
-	def get_code():
-		#method to implement
-		pass
+	def get_code(self):
+		return self.code
 
-	def validate():
-		#method to implement
-		pass
+	def validate(self):
+		return validate(self.code)
+		
 
-	def score():
-		#method to implement
-		pass
+	def score(self):
+		challenge_script = open(self.challenge.code,"r").readlines()
+		repair_script = open(self.code.path,"r").readlines()
+		return nltk.edit_distance(challenge_script,repair_script)
+		
