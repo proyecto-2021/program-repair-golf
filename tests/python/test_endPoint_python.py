bbc2c82edc149cb6db5a0559e2763a11a454ec4b
nachoo <nachogonz2702@yahoo.com.ar>
2021-11-15 20:30:41 -0300

    Fixed example_programs_test file names to not be taked for pytest #310

--- tests/python/test_endPoint_python.py
+++ tests/python/test_endPoint_python.py
@@ -9,7 +9,7 @@ api_url = 'http://localhost:5000/python/api/v1/python-challenges'
 # testing of one post challenge
 def test_post_pythonChallenge(client):
     repair_objective = "make to pass"
-    response = send_post(client, "valid_code_1.py", "valid_test_1.py", repair_objective, "2")
+    response = send_post(client, "valid_code_1.py", "valid_atest_1.py", repair_objective, "2")
 
     assert response.status_code == 200
 
@@ -25,14 +25,14 @@ def test_get_challenge_not_found(client):
 def test_get_single_pythonChallenge(client):
     #---- post one challenge to test ---#    
     repair_objectiveParam = "prueba test"
-    post_info = send_post(client, "valid_code_1.py", "valid_test_1.py", repair_objectiveParam, "3")
+    post_info = send_post(client, "valid_code_1.py", "valid_atest_1.py", repair_objectiveParam, "3")
     
     challenge_id = post_info.json['challenge']['id']
     result = client.get(api_url + '/' + str(challenge_id))
     #---- end post ---#
 
     #data to be entered in the post test
-    post_expected_response = create_expected_response(0, "valid_code_1.py", "3", 'prueba test', "valid_test_1.py")
+    post_expected_response = create_expected_response(0, "valid_code_1.py", "3", 'prueba test', "valid_atest_1.py")
 
     #data obtained through the get ready for manipulation
     dataChallenge = result.json
@@ -53,10 +53,10 @@ def test_get_total_pythonChallenge(client):
     
     #--- start post challenges ---#
     repair_objectiveParamOne = "probando test"
-    send_post(client, "valid_code_1.py", "valid_test_1.py", repair_objectiveParamOne, "1")
+    send_post(client, "valid_code_1.py", "valid_atest_1.py", repair_objectiveParamOne, "1")
     
     repair_objectiveParamTwo = "pruebita test"
-    send_post(client, "valid_code_1.py", "valid_test_1.py", repair_objectiveParamTwo, "2")
+    send_post(client, "valid_code_1.py", "valid_atest_1.py", repair_objectiveParamTwo, "2")
 
     #--- end post challenges ---#
     responsive = client.get(api_url)
@@ -65,7 +65,7 @@ def test_get_total_pythonChallenge(client):
     assert len(data['challenges']) == initial_challenge_len + 2
 
 def test_post_challenge_invalid_code(client):
-    response = send_post(client, "code_not_compile.py", "valid_test_1.py", "Make all tests pass.", "2")
+    response = send_post(client, "code_not_compile.py", "valid_atest_1.py", "Make all tests pass.", "2")
 
     assert response.status_code == 409
     #get json with the error
@@ -73,15 +73,15 @@ def test_post_challenge_invalid_code(client):
     assert json_response['Error'] == 'Syntax error at ' + 'code_not_compile.py'
 
 def test_post_challenge_invalid_test(client):
-    response = send_post(client, "valid_code_1.py", "test_not_compile.py", "Make all tests pass.", "2")
+    response = send_post(client, "valid_code_1.py", "atest_not_compile.py", "Make all tests pass.", "2")
 
     assert response.status_code == 409
     #get json with the error
     json_response = response.json
-    assert json_response['Error'] == 'Syntax error at ' + 'test_not_compile.py'
+    assert json_response['Error'] == 'Syntax error at ' + 'atest_not_compile.py'
 
 def test_post_challenge_binarycode_empty(client):
-    response = send_post(client, "", "valid_test_1.py", "Make all tests pass.", "2")
+    response = send_post(client, "", "valid_atest_1.py", "Make all tests pass.", "2")
     assert response == 400
 
 def test_post_challenge_binarycodetest_empty(client):
@@ -90,7 +90,7 @@ def test_post_challenge_binarycodetest_empty(client):
 
 #post challenge with no errors in tests (so its repaired)
 def test_post_invalid_repaired_challenge(client):
-    response = send_post(client, "code_repair_2.py", "valid_test_2.py", "Make all tests pass.", "2")
+    response = send_post(client, "code_repair_2.py", "valid_atest_2.py", "Make all tests pass.", "2")
 
     assert response.status_code == 409
     #get json with the error
@@ -99,13 +99,13 @@ def test_post_invalid_repaired_challenge(client):
 
 def test_update_simple_fields(client):
     #make a post and save id
-    post_info = send_post(client, "valid_code_1.py", "valid_test_1.py", "Make all tests pass.", "1")
+    post_info = send_post(client, "valid_code_1.py", "valid_atest_1.py", "Make all tests pass.", "1")
     challenge_id = post_info.json['challenge']['id']
     #send an update request
     update_request = request_creator(repair_objective="updated", complexity="3")
     response = client.put(api_url + '/' + str(challenge_id), data=update_request)
 
-    update_expected_response = create_expected_response(0, "valid_code_1.py", "3", 'updated', "valid_test_1.py")
+    update_expected_response = create_expected_response(0, "valid_code_1.py", "3", 'updated', "valid_atest_1.py")
 
     assert response.status_code == 200
     assert response.json == update_expected_response
@@ -114,7 +114,7 @@ def test_update_simple_fields(client):
     assert response.json == update_expected_response
 
 def test_update_valid_code(client):
-    post_info = send_post(client, "valid_code_1.py", "valid_test_1.py", "Make all tests pass.", "1")
+    post_info = send_post(client, "valid_code_1.py", "valid_atest_1.py", "Make all tests pass.", "1")
 
     challenge_id = post_info.json['challenge']['id']
     #send an update request
@@ -126,7 +126,7 @@ def test_update_valid_code(client):
     assert update_response.json['challenge']['code'] == read_file(examples_path + 'valid_code_3.py', 'r')
 
 def test_update_not_compiling_code(client):
-    post_info = send_post(client, "valid_code_1.py", "valid_test_1.py", "Make all tests pass.", "1")
+    post_info = send_post(client, "valid_code_1.py", "valid_atest_1.py", "Make all tests pass.", "1")
 
     challenge_id = post_info.json['challenge']['id']
     #send an update request
@@ -139,7 +139,7 @@ def test_update_not_compiling_code(client):
 
     
 def test_update_repaired_code(client):
-    post_info = send_post(client, "valid_code_1.py", "valid_test_1.py", "Make all tests pass.", "1")
+    post_info = send_post(client, "valid_code_1.py", "valid_atest_1.py", "Make all tests pass.", "1")
 
     challenge_id = post_info.json['challenge']['id']
     #send an update request with repaired code
@@ -151,10 +151,10 @@ def test_update_repaired_code(client):
     assert update_response.json['Error'] == 'At least one test must fail'
 
 def test_update_test_invalid_import(client):
-    post_info = send_post(client, "valid_code_1.py", "valid_test_1.py", "Make all tests pass.", "1")
+    post_info = send_post(client, "valid_code_1.py", "valid_atest_1.py", "Make all tests pass.", "1")
     challenge_id = post_info.json['challenge']['id']
 
-    update_request = request_creator(test_path=examples_path + "import_error_test.py")
+    update_request = request_creator(test_path=examples_path + "import_error_atest.py")
     update_response = client.put(api_url + '/' + str(challenge_id), data=update_request)
 
     assert update_response.status_code == 409
@@ -162,7 +162,7 @@ def test_update_test_invalid_import(client):
     assert update_response.json['Error'] == "Import error, tests can't run"
 
 def test_update_code_name_fails(client):
-    post_info = send_post(client, "valid_code_1.py", "valid_test_1.py", "Make all tests pass.", "1")
+    post_info = send_post(client, "valid_code_1.py", "valid_atest_1.py", "Make all tests pass.", "1")
     challenge_id = post_info.json['challenge']['id']
     
     #updating to a name that already exists
@@ -180,7 +180,7 @@ def test_update_code_name_fails(client):
     assert update_response.json['Error'] == "Import error, tests can't run"
 
 def test_update_code_name_fails(client):
-    post_info = send_post(client, "valid_code_1.py", "valid_test_1.py", "Make all tests pass.", "1")
+    post_info = send_post(client, "valid_code_1.py", "valid_atest_1.py", "Make all tests pass.", "1")
     challenge_id = post_info.json['challenge']['id']
     
     #updating to a name that already exists
@@ -198,25 +198,25 @@ def test_update_code_name_fails(client):
     assert update_response.json['Error'] == "Import error, tests can't run"
 
 def test_update_all(client):
-    post_info = send_post(client, "valid_code_1.py", "valid_test_1.py", "Make all tests pass.", "1")
+    post_info = send_post(client, "valid_code_1.py", "valid_atest_1.py", "Make all tests pass.", "1")
     challenge_id = post_info.json['challenge']['id']
 
     update_request = request_creator(code_path=examples_path + "code_change_my_name_4.py", 
-    test_path=examples_path + "test_change_my_name_4.py", code_name="unique_code_1.py", test_name="unique_test_1.py")
+    test_path=examples_path + "atest_change_my_name_4.py", code_name="unique_code_1.py", test_name="unique_atest_1.py")
 
     update_response = client.put(api_url + '/' + str(challenge_id), data=update_request)
 
     assert update_response.status_code == 200
 
     assert read_file("public/challenges/unique_code_1.py", "r") == update_response.json['challenge']['code']
-    assert read_file("public/challenges/unique_test_1.py", "r") == update_response.json['challenge']['tests_code']
+    assert read_file("public/challenges/unique_atest_1.py", "r") == update_response.json['challenge']['tests_code']
 
 def test_update_all_code_name_conflict(client):
-    post_info = send_post(client, "valid_code_1.py", "valid_test_1.py", "Make all tests pass.", "1")
+    post_info = send_post(client, "valid_code_1.py", "valid_atest_1.py", "Make all tests pass.", "1")
     challenge_id = post_info.json['challenge']['id']
     #code name is not the same as the new test is importing
     update_request = request_creator(code_path=examples_path + "code_change_my_name_4.py", 
-    test_path=examples_path + "test_change_my_name_4.py", code_name="test_will_not_find_me.py", test_name="unique_test_1.py")
+    test_path=examples_path + "atest_change_my_name_4.py", code_name="test_will_not_find_me.py", test_name="unique_atest_1.py")
 
     update_response = client.put(api_url + '/' + str(challenge_id), data=update_request)
 
@@ -226,7 +226,7 @@ def test_update_all_code_name_conflict(client):
 def test_post_repair_challenge(client):
 
     repair_objectiveParam = "Test repair"
-    post_info = send_post(client, "valid_code_1.py", "valid_test_1.py", repair_objectiveParam, '3')
+    post_info = send_post(client, "valid_code_1.py", "valid_atest_1.py", repair_objectiveParam, '3')
 
     challenge_id = post_info.json['challenge']['id']
 
@@ -252,7 +252,7 @@ def test_post_repair_challenge_invalid(client):
 def test_post_repair_code_not_provided(client):
     
     repair_objectiveParam = "Test repair"
-    post_info = send_post(client, "valid_code_1.py", "valid_test_1.py", repair_objectiveParam, '3')
+    post_info = send_post(client, "valid_code_1.py", "valid_atest_1.py", repair_objectiveParam, '3')
     challenge_id = post_info.json['challenge']['id']
     
     response = client.post(api_url + '/' + str(challenge_id) + '/repair')
@@ -263,7 +263,7 @@ def test_post_repair_code_not_provided(client):
 def test_post_repair_update_best_score(client):
 
     repair_objectiveParam = "Test repair"
-    post_info = send_post(client, "valid_code_1.py", "valid_test_1.py", repair_objectiveParam, '3')
+    post_info = send_post(client, "valid_code_1.py", "valid_atest_1.py", repair_objectiveParam, '3')
 
     challenge_id = post_info.json['challenge']['id']
 
@@ -282,11 +282,11 @@ def test_post_repair_update_best_score(client):
 def test_post_repair_not_past_tests(client):
     
     repair_objectiveParam = "Test repair"
-    post_info = send_post(client, "valid_code_1.py", "valid_test_1.py", repair_objectiveParam, '3')
+    post_info = send_post(client, "valid_code_1.py", "valid_atest_1.py", repair_objectiveParam, '3')
 
     challenge_id = post_info.json['challenge']['id']
 
-    repair_request = request_creator(code_path = examples_path + "code_repair_test_fail.py")
+    repair_request = request_creator(code_path = examples_path + "code_repair_atest_fail.py")
 
     response = client.post(api_url + '/' + str(challenge_id) + '/repair', data = repair_request)
 
