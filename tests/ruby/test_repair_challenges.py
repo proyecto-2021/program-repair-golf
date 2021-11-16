3352fd1cd9f0464a76580ab721a033c2ca745e58
Agustin Nolasco <franconolasco99@gmail.com>
2021-11-15 23:54:31 -0300

    Standarize var names. Ref #281

--- tests/ruby/test_repair_challenges.py
+++ tests/ruby/test_repair_challenges.py
@@ -3,10 +3,10 @@ from .data_generator import get_data
 
 def test_post_repair(client):
     #arrange
-    url = '/ruby/challenge'
+    url1 = '/ruby/challenge'
     data = get_data('example4', 'example_test4', 'Testing repair', '3', 'example_challenge', 'example_test4')
-    r = client.post(url, data=data)
-    id = r.json['challenge']['id']
+    r1 = client.post(url1, data=data)
+    id = r1.json['challenge']['id']
     url2 = f'/ruby/challenge/{id}/repair'
     data2 = { 'source_code_file': open('tests/ruby/tests-data/example_fixed.rb', 'rb') }
 
@@ -14,7 +14,7 @@ def test_post_repair(client):
     r2 = client.post(url2, data=data2)
 
     #assert
-    assert r.status_code == 200
+    assert r1.status_code == 200
     assert r2.status_code == 200
     assert r2.json['repair']['score'] != 0
 
@@ -32,10 +32,10 @@ def test_post_repair_invalid_challenge(client):
 
 def test_post_repair_candidate_compiles_error(client):
     #arrange
-    url = '/ruby/challenge'
+    url1 = '/ruby/challenge'
     data = get_data('example10', 'example_test10', 'Testing repair candidate does not compile', '3', 'example_challenge', 'example_test10')
-    r = client.post(url, data=data)
-    id = r.json['challenge']['id']
+    r1 = client.post(url1, data=data)
+    id = r1.json['challenge']['id']
     url2 = f'/ruby/challenge/{id}/repair'
     data2 = {'source_code_file': open('tests/ruby/tests-data/example_fixed_no_compiles.rb', 'rb')}
 
@@ -43,16 +43,16 @@ def test_post_repair_candidate_compiles_error(client):
     r2 = client.post(url2, data=data2)
 
     #assert
-    assert r.status_code == 200
+    assert r1.status_code == 200
     assert r2.status_code == 400
     assert r2.json['challenge'] == 'the repair candidate has syntax errors'
 
 def test_post_repair_candidate_tests_fail(client):
     #arrange
-    url = '/ruby/challenge'
+    url1 = '/ruby/challenge'
     data = get_data('example11', 'example_test11', 'Testing repair candidate fail tests', '2', 'example_challenge', 'example_test11')
-    r = client.post(url, data=data)
-    id = r.json['challenge']['id']
+    r1 = client.post(url1, data=data)
+    id = r1.json['challenge']['id']
     url2 = f'/ruby/challenge/{id}/repair'
     data2 = { 'source_code_file': open('tests/ruby/tests-data/example_challenge.rb', 'rb') }
 
@@ -60,22 +60,22 @@ def test_post_repair_candidate_tests_fail(client):
     r2 = client.post(url2, data=data2)
 
     #assert
-    assert r.status_code == 200
+    assert r1.status_code == 200
     assert r2.status_code == 200
     assert r2.json['challenge'] == 'the repair candidate does not solve the problem'
 
 def test_post_repair_without_repair_candidate(client):
     #arrange
-    url = '/ruby/challenge'
+    url1 = '/ruby/challenge'
     data = get_data('example22', 'example_test22', 'Testing post repair without code', '4', 'example_challenge', 'example_test22')
-    r = client.post(url, data=data)
-    id = r.json['challenge']['id']
+    r1 = client.post(url1, data=data)
+    id = r1.json['challenge']['id']
     url2 = f'/ruby/challenge/{id}/repair'
 
     #act
     r2 = client.post(url2)
 
     #assert
-    assert r.status_code == 200
+    assert r1.status_code == 200
     assert r2.status_code == 400
     assert r2.json['challenge'] == 'a repair candidate is necessary'
