6ee207ef4ae82b5549a4c7659c6d36d19350dc6c
EAMolina500 <molinaenzoagustin@gmail.com>
2021-11-16 11:21:20 -0300

    merge version. Ref #268

--- app/go/go_repair_candidate.py
+++ app/go/go_repair_candidate.py
@@ -1,5 +1,7 @@
 from nltk import edit_distance
 from .go_src import Go_src
+from .go_challenge import GoChallenge
+import os
 
 class GoRepairCandidate:
 	def __init__(self, challenge=None, repair_code=None):
@@ -8,23 +10,22 @@ class GoRepairCandidate:
 
 	def get_content_repair(self, score):
 		return {
-        	'repair_objective': self.challenge.get_repair_objective(),
-			'best_score': self.best_score.get_best_score(),
+			'repair_objective': self.challenge.get_repair_objective(),
+			'best_score': self.challenge.get_best_score(),
 			'player': {'username': 'Moli'},
 			'attemps': 1,
 			'score': score
-       	}
+		}
 
 	def score(self):
-		return nltk.edit_distance(self.repair_code.get_code_content(), self.challenge.code.get_code_content())
+		return edit_distance(self.repair_code.get_content(), self.challenge.get_code_content())
 
 	def code_compiles(self):
 		return self.repair_code.code_compiles()
 
 	def tests_fail(self):
 		tests = Go_src(path=self.challenge.get_tests_code())
-    	return tests.tests_fail()
+		return tests.tests_fail()
 
-    def save(self):
-    	self.repair_code.save()
-    	
+	def save(self):
+		self.repair_code.save()
