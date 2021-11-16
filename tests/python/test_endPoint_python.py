42e02fc25f24bc10c3b6940e5e4b534ae17087d2
matiasg99 <matiias.gomeez999@gmail.com>
2021-11-15 22:33:16 -0300

    separate utils  #310

--- tests/python/test_endPoint_python.py
+++ tests/python/test_endPoint_python.py
@@ -1,10 +1,8 @@
 from app.python.models import *
 from . import client
-from app.python.file_utils import read_file
+from .atest_utils import *
 import json
 
-examples_path = 'tests/python/example_programs_test/'
-api_url = 'http://localhost:5000/python/api/v1/python-challenges'
 
 # testing of one post challenge
 def test_post_pythonChallenge(client):
@@ -301,73 +299,3 @@ def test_post_repair_not_past_tests(client):
     assert response.status_code == 409
     assert response.json['Error'] == "Some test failed"
 
-# -------Section functions ------- #
-def request_creator(**params):
-    #we check each param's presence and add it to dataChallenge
-    dataChallenge = {}
-    #file checking
-    if params.get('code_path') is not None:
-        dataChallenge['source_code_file'] = open(params.get('code_path'), 'rb')
-    if params.get('test_path') is not None:
-        dataChallenge['test_suite_file'] = open(params.get('test_path'), 'rb')
-    
-    #creating challenge data as string
-    challenge_str = '{ "challenge": { '
-    comma_needed = False
-    challenge_str, comma_needed = check_and_concatenate(challenge_str, '"source_code_file_name" : "', params.get('code_name'), comma_needed)
-    challenge_str, comma_needed = check_and_concatenate(challenge_str, '"test_suite_file_name" : "', params.get('test_name'), comma_needed)
-    challenge_str, comma_needed = check_and_concatenate(challenge_str, '"repair_objective" : "', params.get('repair_objective'), comma_needed)
-    challenge_str, comma_needed = check_and_concatenate(challenge_str, '"complexity" : "', params.get('complexity'), comma_needed)
-    challenge_str += '} }'
-
-    if comma_needed: #comma needed, thus at least one parameter was required
-        dataChallenge['challenge'] = challenge_str
-
-    return dataChallenge     
-# ------- end Section functions ------- #
-
-def check_and_concatenate(base_str, base_addition, addition, comma_needed):
-    if addition is None: return base_str, comma_needed #nothing to be added
-    if comma_needed: base_str += ', '
-    comma_needed = True
-
-    base_str += base_addition + addition + '"'    #concatenate strings
-    return base_str, comma_needed   #return both string and comma_needed
-
-def create_expected_response(best_score, code_name, complexity, repair_objective, test_name):
-    code = read_file(examples_path + code_name, 'r')
-    test = read_file(examples_path + test_name, 'r')
-    expected_response = {
-        'challenge': {
-            'best_score': best_score,
-            'code': code,
-            'complexity': complexity,
-            'repair_objective': repair_objective,
-            'tests_code': test
-        }
-    }
-    return expected_response
-
-def send_post(client, code_name, test_name, repair_objective, complexity):
-    
-    if isBlank(code_name):
-        return 400
-    else:
-        code_path = examples_path + code_name
-    
-    if isBlank(test_name):
-        return 400
-    else:
-        test_path = examples_path + test_name
-    
-    dataChallengePost = request_creator(code_path=code_path, test_path=test_path, code_name=code_name,
-    test_name=test_name, repair_objective=repair_objective, complexity=complexity)
-
-    return client.post(api_url, data=dataChallengePost)
-
-def isBlank (myString):
-    if myString and myString.strip():
-        #myString is not None AND myString is not empty or blank
-        return False
-    #myString is None OR myString is empty or blank
-    return True
