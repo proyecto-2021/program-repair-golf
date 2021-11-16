fd67102e1377aec0c2b4c00dd7639b03ad789168
Luchit0-afk <lucianopalacio91@gmail.com>
2021-11-16 15:00:21 -0300

    Add a method for return access token in the test. Ref #370

--- tests/go/__init__.py
+++ tests/go/__init__.py
@@ -14,6 +14,17 @@ def client():
             db.create_all()
             # Tests will be executed on the test_client object
             yield test_client
+            
+@pytest.fixture(scope='module')
+def auth(client):
+    user = {'username': 'user', 'password': 'root'}
+    
+    ret_post_user = client.post("/users", json=user)
+    ret_post_auth = client.post("/auth", json=user)
+    
+    acces_token = ret_post_auth.json["access_token"]
+    
+    return acces_token
 
 
     
