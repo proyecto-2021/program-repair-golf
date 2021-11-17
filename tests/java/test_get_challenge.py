6b43ad334f9c7ccb26bf05f7f3f1e1bf6dff7510
virginiacattaneo <82518006+virginiacattaneo@users.noreply.github.com>
2021-11-17 14:58:13 -0300

    Update test get challenge

--- tests/java/test_get_challenge.py
+++ tests/java/test_get_challenge.py
@@ -9,6 +9,7 @@ from tests.java import data_for_tests
 
 def test_get_Id_after_post(client):
 	db.session.query(Challenge_java).delete()
+    #arrange
 	url = 'http://localhost:5000/java/java-challenges'
 	data = data_for_tests.createChallenge('example-challenges/java-challenges/Median.java','example-challenges/java-challenges/MedianTest.java','Median','MedianTest', 'pass', '1')
 	token=data_for_tests.get_token(client)
@@ -18,32 +19,36 @@ def test_get_Id_after_post(client):
 	id = json['id']
 
 	url2 = f'http://localhost:5000/java/java-challenges/{id}'
+    #act
 	p2 = client.get(url2, headers={'Authorization': f'JWT {token}'})
 	json2 = p2.json['challenge']
 	id2=json2['id']
-
+    #assert
 	assert p.status_code == 200
 	assert p2.status_code == 200
 	assert id == id2
 
-def test_get_Id_noesxite(client):
-	data_for_tests.delete_db()
-	id=1
-	url2 = f'http://localhost:5000/java/java-challenges/{id}'
-	token = data_for_tests.get_token(client)
-	p2 = client.get(url2, headers={'Authorization': f'JWT {token}'})
-	
-	assert p2.status_code == 404
-
 def test_get_Id_novalido(client):
-	db.session.query(Challenge_java).delete()
-	id='hola' 
-	url2 = f'http://localhost:5000/java/java-challenges/{id}'
-	token = data_for_tests.get_token(client)
-	p2 = client.get(url2, headers={'Authorization': f'JWT {token}'})
-	
-	assert p2.status_code == 404
+    db.session.query(Challenge_java).delete()
+    #arr
+    id='hola' 
+    url2 = f'http://localhost:5000/java/java-challenges/{id}'
+    token=data_for_tests.get_token(client)
+    #act
+    p2 = client.get(url2, headers={'Authorization': f'JWT {token}'})
+	#assert
+    assert p2.status_code == 404
 
+def test_get_Id_noesxite(client):
+    db.session.query(Challenge_java).delete()
+    #arr
+    id=1
+    url2 = f'http://localhost:5000/java/java-challenges/{id}'
+    token=data_for_tests.get_token(client)
+    #act
+    p2 = client.get(url2, headers={'Authorization': f'JWT {token}'})
+	#assert
+    assert p2.status_code == 404
 def test_get_java(client):
 	data_for_tests.delete_db()
 	url = 'http://localhost:5000/java/java-challenges'
