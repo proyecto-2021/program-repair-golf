d5becf7970a4739706c4276f11770bf2ff35836b
Alan <alan_arce97@hotmail.com>
2021-11-18 11:24:29 -0300

    post method in views ready. Ref #263

--- app/go/views.py
+++ app/go/views.py
@@ -17,70 +17,21 @@ controller = Controller()
 def repair_challenge_go(id):
     return controller.post_repair(id)
 
-
-
 @go.route('/api/v1/go-challenges', methods=['GET'])
 def get_challenges():
     return controller.get_all_challenges()
-
-
 
 @go.route('/api/v1/go-challenges/<id>', methods=['GET'])
 def get_challenge(id):
     return controller.get_challenge_by_id(id)
 
-
 @go.route('/api/v1/go-challenges/<id>', methods=['PUT'])
 def update_a_go_challenge(id):
     return controller.update_a_go_challenge(id)
     
-
 @go.route('/api/v1/go-challenges', methods=['POST'])
 def create_go_challenge():
-    challenge_data = json.loads(request.form.get('challenge'))['challenge']
-
-    code_file = request.files["source_code_file"]
-    code_path = 'public/challenges/' + challenge_data['source_code_file_name']
-    code_file.save(code_path)
-
-    test_suite_file = request.files["test_suite_file"]
-    test_suite_path = 'public/challenges/' + challenge_data['test_suite_file_name']
-    test_suite_file.save(test_suite_path)
-
-    new_challenge = GoChallenge(
-        code=code_path,
-        tests_code=test_suite_path,
-        repair_objective=challenge_data['repair_objective'],
-        complexity=challenge_data['complexity'],
-        best_score=math.inf)
-
-    #all_the_challenges = db.session.query(GoChallenge).all()
-    all_the_challenges = dao.get_all_challenges()
-    for every_challenge in all_the_challenges:
-        if every_challenge.code == new_challenge.code:
-            return make_response(jsonify({"challenge": "repeated"}), 409)
-
-    test_pass = subprocess.run(["go" "test"], cwd=os.path.abspath("public/challenges"), shell=True)
-    test_compilation = subprocess.run(["go" "test" "-c"], cwd=os.path.abspath("public/challenges"),shell=True)
-    code_compilation = subprocess.run(["go" "build", code_path], stderr=subprocess.STDOUT, stdout=subprocess.DEVNULL,shell=True)
-
-    codigo = Go_src(path=code_path)
-    tests = Go_src(path="public/challenges")
-
-    if code_compilation == 1:
-        return make_response(jsonify({"code_file": "The code has syntax errors"}), 412)
-    elif test_compilation == 1 or test_compilation.returncode == 2:
-        return make_response(jsonify({"test_code_file": "The test code has syntax errors"}), 412)
-    elif test_pass.returncode == 0:
-        return make_response(jsonify({"ERROR: tests": "There must be at least one test that fails"}), 412)
-
-    db.session.add(new_challenge)
-    db.session.commit()
-
-    new_challenge_to_dicc = new_challenge.convert_dict()
-    from_file_to_str(new_challenge_to_dicc, "code")
-    from_file_to_str(new_challenge_to_dicc, "tests_code")
-    return jsonify({"challenge": new_challenge_to_dicc})
+    return controller.post_challenge()
 
 
 def from_file_to_str(challenge, attribute):
