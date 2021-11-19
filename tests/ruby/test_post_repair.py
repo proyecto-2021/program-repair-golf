1d77f69155d07c7844827c2b3df4a45ae7879dac
Agustin Nolasco <franconolasco99@gmail.com>
2021-11-19 19:40:18 -0300

    Add new test to post repair, check if the attemps increase properly. Ref #396

--- tests/ruby/test_post_repair.py
+++ tests/ruby/test_post_repair.py
@@ -2,7 +2,7 @@ from . import client, auth, generic_post
 from nltk import edit_distance
 
 
-def test_post_repair(client, auth, generic_post):
+def test_post_repair1(client, auth, generic_post):
     # arrange
     orig_json = generic_post['challenge'].copy()
     challenge_id = orig_json['id']
@@ -18,7 +18,38 @@ def test_post_repair(client, auth, generic_post):
     assert r.status_code == 200
     assert r.json['repair']['score'] == edit_distance(orig_json['code'], code_fixed_content)
     assert r.json['repair']['player']['username'] == 'ruby'
-    assert r.json['repair']['attempts'] == '1'
+
+
+def test_post_repair2(client, auth, generic_post):
+    # arrange
+    orig_json = generic_post['challenge'].copy()
+    challenge_id = orig_json['id']
+    url = f'/ruby/challenge/{challenge_id}/repair'
+    data1 = {'source_code_file': open('tests/ruby/tests-data/example_fixed1.rb', 'rb')}
+    data2 = {'source_code_file': open('tests/ruby/tests-data/example_not_compiles.rb', 'rb')}
+    data3 = {'source_code_file': open('tests/ruby/tests-data/example_fixed2.rb', 'rb')}
+
+    # act
+    r1 = client.post(url, data=data1, headers={'Authorization': f'JWT {auth}'})
+    with open('tests/ruby/tests-data/example_fixed1.rb') as f:
+        code_fixed_content1 = f.read()
+
+    r2 = client.post(url, data=data2, headers={'Authorization': f'JWT {auth}'})
+
+    r3 = client.post(url, data=data3, headers={'Authorization': f'JWT {auth}'})
+    with open('tests/ruby/tests-data/example_fixed2.rb') as f:
+        code_fixed_content2 = f.read()
+
+    # assert
+    assert r1.status_code == 200
+    assert r1.json['repair']['score'] == edit_distance(orig_json['code'], code_fixed_content1)
+    assert r1.json['repair']['player']['username'] == 'ruby'
+    assert r2.status_code == 400
+    assert r2.json['challenge'] == 'the repair candidate has syntax errors'
+    assert r3.status_code == 200
+    assert r3.json['repair']['score'] == edit_distance(orig_json['code'], code_fixed_content2)
+    assert r3.json['repair']['player']['username'] == 'ruby'
+    assert str(int(r1.json['repair']['attempts']) + 2) == r3.json['repair']['attempts']
 
 
 def test_post_repair_invalid_challenge(client, auth):
