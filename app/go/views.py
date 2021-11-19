72ff00679ba935674769ccc7ab9697ed2d40b708
Luchit0-afk <lucianopalacio91@gmail.com>
2021-11-19 13:39:25 -0300

    Add a jwt_required() in the file views. Ref #370

--- app/go/views.py
+++ app/go/views.py
@@ -7,27 +7,33 @@ import os, subprocess, math, nltk, shutil
 from .go_challenge_dao import goChallengeDAO
 from .go_src import Go_src
 from .go_controller import Controller
+from flask_jwt import jwt_required
 
 
 dao = goChallengeDAO()
 controller = Controller()
 
 @go.route('api/v1/go-challenges/<int:id>/repair', methods=['POST'])
+@jwt_required()
 def repair_challenge_go(id):
     return controller.post_repair(id)
 
 @go.route('/api/v1/go-challenges', methods=['GET'])
+@jwt_required()
 def get_challenges():
     return controller.get_all_challenges()
 
 @go.route('/api/v1/go-challenges/<id>', methods=['GET'])
+@jwt_required()
 def get_challenge(id):
     return controller.get_challenge_by_id(id)
 
 @go.route('/api/v1/go-challenges/<id>', methods=['PUT'])
+@jwt_required()
 def update_a_go_challenge(id):
     return controller.update_a_go_challenge(id)
     
 @go.route('/api/v1/go-challenges', methods=['POST'])
+@jwt_required()
 def create_go_challenge():
     return controller.post_challenge()
