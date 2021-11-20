412e41d8adb9729f11a6cdd49e44094ad5bbf9f5
Jplferreyra <jplferreyra@gmail.com>
2021-11-20 18:57:45 -0300

    Normalize docstring syntaxis. Ref #393

--- app/ruby/rubycode.py
+++ app/ruby/rubycode.py
@@ -8,14 +8,15 @@ class RubyCode:
     """Provide handling of the given code"""
     def __init__(self, full_name=None):
         """Initialize code with or without full name.
+        
         Parameters:
-            full_name (string): the name of the code with its path.
+            full_name (str): the name of the code with its path.
 
         Attributes:
-            path (string): set where the file are stored,
-            file_name (string): set the name of the code,
-            file (FileStorage): content of the file
-            full_name (string): set the name of the code with its path
+            path (str): set where the file are stored,
+            file_name (str): set the name of the code,
+            file (FileStorage): content of the file,
+            full_name (str): set the name of the code with its path.
         """
         self.path = None
         self.file_name = None
@@ -31,15 +32,15 @@ class RubyCode:
         """Set the code attributes.
 
         Parameters:
-            path (string): is where the files will be stored,
-            file_name (string): the name of the file,
-            file (FileStorage):  the content of the file,
+            path (str): is where the files will be stored,
+            file_name (str): the name of the file,
+            file (FileStorage):  the content of the file.
         
         Attributes:
-            path (string): set where the files will be stored,
-            file_name (string): set name of the file,
+            path (str): set where the files will be stored,
+            file_name (str): set name of the file,
             file (FileStorage): set the content of the file,
-            full_name (string): set the full name of the file.
+            full_name (str): set the full name of the file.
         """
         self.path = path
         self.file_name = file_name
@@ -50,7 +51,7 @@ class RubyCode:
         """Obtain the file path.
 
         Returns:
-            path (string): get the file path.
+            path (str): get the file path.
         """
         return self.path
 
@@ -58,7 +59,7 @@ class RubyCode:
         """Obtain the file name.
 
         Returns:
-            file_name (string): get the file name.
+            file_name (str): get the file name.
         """
         return self.file_name
 
@@ -66,23 +67,23 @@ class RubyCode:
         """Obtain the file full name.
 
         Returns:
-            full_name (string): get the file full name.
+            full_name (str): get the file full name.
         """
         return self.full_name
 
     def file_name_ok(self):
-        """Check if the file name is valid
+        """Check if the file name is valid.
         
         Returns:
-            Bool: confirmation that the file name is not empty or doesnt contain whitespace characters
+            bool: confirmation that the file name is not empty or doesnt contain whitespace characters.
         """
         return self.file_name and self.file_name == self.file_name.strip()
 
     def save(self):
-        """Try to save the file
+        """Try to save the file.
 
         Returns:
-            Bool: return true if the file could be saved and false if the file already exist
+            bool: return true if the file could be saved and false if the file already exist.
         """
         if os.path.isfile(self.get_full_name()):
             return False
@@ -93,12 +94,12 @@ class RubyCode:
         """Move the file to a given path.
 
         Parameters:
-            path (string): is where the files will be moved,
-            names_match (Bool): is a condition that if an element with the same name already exists,
+            path (str): is where the files will be moved,
+            names_match (bool): is a condition that if an element with the same name already exists,
             do not move this file.
 
         Returns:
-            Bool: report if the file could be moved or not.
+            bool: report if the file could be moved or not.
         """
         dst = path + self.get_file_name() + '.rb'
         if not names_match:
@@ -112,10 +113,10 @@ class RubyCode:
         """Create a new file equal to this file in the given path.
 
         Parameters:
-            path (string): is where the files will be stored.
+            path (str): is where the files will be stored.
 
         Returns:
-            string: the new file full name.
+            str: the new file full name.
         """
         dst = path + self.get_file_name() + '.rb'
         return copy(self.get_full_name(), dst)
@@ -124,25 +125,25 @@ class RubyCode:
         """Change the file name.
 
         Parameters:
-            new_name (string): the new name that the file will have.
+            new_name (str): the new name that the file will have.
 
         Attributes:
-        file_name (string): is where the new name is stored,
-        full_name (string): is where the new name is stored with the same path.
+            file_name (str): is where the new name is stored,
+            full_name (str): is where the new name is stored with the same path.
         """
         os.rename(self.get_full_name(), self.path + new_name + '.rb')
         self.file_name = new_name
         self.full_name = self.path + new_name + '.rb'
 
     def remove(self):
-        """delete the file"""
+        """Delete the file"""
         os.remove(self.get_full_name())
 
     def get_content(self):
         """Obtain the file content.
 
         Returns:
-            string: the content file.
+            str: the content file.
         """
         with open(self.get_full_name()) as f:
             return f.read()
@@ -151,7 +152,7 @@ class RubyCode:
         """Try to compile the code.
 
         Returns:
-            Bool: report if the file could be compiled or not
+            bool: report if the file could be compiled or not.
         """
         command = 'ruby -c ' + self.get_full_name()
         return subprocess.call(command, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT) == 0
@@ -160,7 +161,7 @@ class RubyCode:
         """Check that the file does not run correctly.
 
         Returns:
-            Bool: report if the file could not be executed
+            bool: report if the file could not be executed.
         """
         command = 'ruby ' + self.get_full_name()
         return subprocess.call(command, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT) != 0
@@ -175,7 +176,7 @@ class RubyTestsCode(RubyCode):
             code (RubyCode): the code to which you want to apply the test suite.
 
         Returns:
-            Bool: report if the test suite can be apply to the code.
+            bool: report if the test suite can be apply to the code.
         """
         command = 'grep "require_relative" ' + self.get_full_name()
         p = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
