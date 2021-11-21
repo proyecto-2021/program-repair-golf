d337595b83a9d09709494e08c57e19f08134c464
Mati36 <lopezmatias36@gmail.com>
2021-11-21 20:50:42 -0300

    test for after post Ref #361

--- tests/javascript/__init__.py
+++ tests/javascript/__init__.py
@@ -1,6 +1,7 @@
 from app import create_app, db
 import pytest
-from app.javascript.controllers.files_controller import open_file
+from app.javascript.controllers.files_controller import open_file, remove_files
+from app.javascript.dao.challenge_dao import ChallengeDAO
 
 @pytest.fixture(scope='module')
 def client():
@@ -22,21 +23,36 @@ def auth(client):
     token = r.json['access_token']
     return token
 
+
+@pytest.fixture(scope='module')
+def post_generator(client, auth):
+    data = createChallenge('example0', 'example0.test', 'Testing', '1')
+    r = client.post('javascript/javascript-challenges', data=data, headers={'Authorization': f'JWT {auth}'})
+    return r.json
+
 #importar open file
 def createChallenge( code_name, test_name,objective,complexity):
     code_path = f'example-challenges/javascript-challenges/{code_name}.js'
     test_path = f'example-challenges/javascript-challenges/{test_name}.js'
-
+    
     challenge = {
-		'source_code_file': open_file(code_path),
-		'test_suite_file': open_file(test_path),
-		f'challenge':'{ \
+		'source_code_file': open(code_path, 'rb'),
+		'test_suite_file': open(test_path, 'rb'),
+		'challenge':'{ \
             "challenge":{\
-                "source_code_file_name":{code_name},\
-                "test_suite_file_name": {test_name},\
-                "repair_objective": {objective} ,\
-                "complexity": {complexity}\
+                "source_code_file_name": "median",\
+                "test_suite_file_name": "median.test",\
+                "repair_objective": "objective",\
+                "complexity": "number"\
             }\
         }'
 	}
+
+    challenge['challenge'] = challenge['challenge'].replace('median', code_name)
+    challenge['challenge'] = challenge['challenge'].replace('median.test', test_name)
+    challenge['challenge'] = challenge['challenge'].replace('objetive', objective)
+    challenge['challenge'] = challenge['challenge'].replace('number', complexity)
+
+
     return challenge
+
--- tests/javascript/test_get_challenge.py
+++ tests/javascript/test_get_challenge.py
@@ -1,7 +1,8 @@
 import pytest
 from . import client, auth 
-from . __init__ import createChallenge
-import json
+from . __init__ import createChallenge, post_generator,remove_files, ChallengeDAO
+from . import db
+from app.javascript.models_js import JavascriptChallenge
 
 #Test de hacer un get luego de hacer un post
 
@@ -14,6 +15,7 @@ def test_challenge_id_not_valid(client,auth):
     #assert
     assert resp.status_code == 404
     #assert resp.json['error'] == 'Id challenge not exist'
+    
 def test_get_javascript(client,auth):
     #arrange
 	url = '/javascript-challenges'
@@ -23,7 +25,16 @@ def test_get_javascript(client,auth):
 	#assert
 	assert resp.status_code == 404
 
+def test_get_after_post(client, auth, post_generator):
+    # arrange
+    orig_json = post_generator['Challenge'].copy()
+    challenge_id = orig_json['id']
+    orig_json.pop('id')
+    challenge = ChallengeDAO.get_challenge(challenge_id)
+    
+    # act
+    r = client.get(f'javascript/javascript-challenges/{challenge_id}', headers={'Authorization': f'JWT {auth}'})
+    remove_files(challenge.code, challenge.tests_code)
 
-
-
-
+    # assert
+    assert r.status_code == 200
