c6282ebf0fd5ee215be0b4c5efe4617c1239b9c9
Gonzalo-FloresM <floresgo42@gmail.com>
2021-11-20 22:48:06 -0300

    add a challenge example. Ref #419

---
+++ example-challenges/ruby-challenges/fibonacci_test.rb
@@ -0,0 +1,19 @@
+require 'minitest/autorun'
+require_relative 'fibonacci'
+
+class FibonacciTest < Minitest::Test
+  def test_1
+    assert fibonacci(0) == 1
+  end
+
+  def test_2
+    assert fibonacci(1) == 1
+  end
+
+  def test_3
+    assert fibonacci(2) == 3      
+  end
+
+  def test_4
+    assert fibonacci(8) == 34
+  end
