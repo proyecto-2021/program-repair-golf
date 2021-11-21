2516ff224a26d1ce3749ab6725a840a01f363455
Gonzalo-FloresM <floresgo42@gmail.com>
2021-11-20 23:18:12 -0300

    add factorial example. Ref #419

---
+++ example-challenges/ruby-challenges/factorial_test.rb
@@ -0,0 +1,20 @@
+require 'minitest/autorun'
+require_relative 'factorial'
+
+class FactorialTest < Minitest::Test
+  def test_1
+    assert factorial(0) == 0
+  end
+
+  def test_2
+    assert factorial(1) == 1
+  end
+
+  def test_3
+    assert factorial(3) == 6
+  end
+
+  def test_4
+    assert factorial(10) == 3628800
+  end
+end
