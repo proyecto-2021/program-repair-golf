9dec1002ed022286fd7bd01f837c5299296a4c7d
AgustinCalderon <agustincalderon058@gmail.com>
2021-11-22 11:23:10 -0300

    Added new table to repair attempts #377

--- app/python/models.py
+++ app/python/models.py
@@ -1,4 +1,11 @@
 from .. import db
+from sqlalchemy.sql.schema import CheckConstraint
+
+python_repair_attempt = db.Table('python_repair_attempt',
+  db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True),
+  db.Column('challenge_id', db.Integer, db.ForeignKey('python_challenge_model.id'), primary_key=True),
+  db.Column('attempts', db.Integer, default=0)
+)
 
 class PythonChallengeModel(db.Model):
   id = db.Column(db.Integer, primary_key=True)
@@ -7,6 +14,8 @@ class PythonChallengeModel(db.Model):
   repair_objective = db.Column(db.String(120), nullable=False)
   complexity = db.Column(db.Integer, nullable=False)
   best_score = db.Column(db.Integer, nullable=True)
+  rapair_attempt = db.relationship("User", secondary=python_repair_attempt)
+
 
   #takes a row and returns it as a dictionary
   def to_dict(self):
